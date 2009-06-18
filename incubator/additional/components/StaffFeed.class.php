b7c1d52bcbb87dfa7198c31c958568a8687f19a9
drPavka <d.pavka@gmail.com>
2009-06-18 12:02:10 +0000

    Добавлен набор компонентов для отображения списка сотрудников
    git-svn-id: https://energine.googlecode.com/svn/trunk@125 acdd3125-bb50-0410-93b3-95ffde4b7c7d

---
+++ incubator/additional/components/StaffFeed.class.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Содержит класс StaffFeed.
+ *
+ * @package energine
+ * @subpackage aux
+ * @author d.pavka
+ * @copyright d.pavka@gmail.com
+ */
+
+ /**
+ * Список сотрудников
+ *
+ * @package energine
+ * @subpackage aux
+ * @author d.pavka
+ */
+ class StaffFeed extends Feed {
+    /**
+     * Конструктор класса
+     *
+     * @param string $name
+     * @param string $module
+     * @param Document $document
+     * @param array $params
+     * @access public
+     */
+    public function __construct($name, $module, Document $document,  array $params = null) {
+        parent::__construct($name, $module, $document,  $params);
+        $this->setTableName('aux_staff');
+        $this->setOrder(array('staff_order_num' => QAL::DESC));
+    }
+}

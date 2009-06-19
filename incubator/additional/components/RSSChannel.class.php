4814ae27d0b43b2b31183dadee5bb9d6f9ae5902
drPavka <d.pavka@gmail.com>
2009-06-19 12:35:32 +0000

    Заготовки Issue #57
    git-svn-id: https://energine.googlecode.com/svn/trunk@130 acdd3125-bb50-0410-93b3-95ffde4b7c7d

---
+++ incubator/additional/components/RSSChannel.class.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Содержит класс RSSSChannel
+ *
+ * @package energine
+ * @subpackage aux
+ * @author d.pavka
+ * @copyright d.pavka@gmail.com
+ */
+
+ /**
+  * Генератор RSS feed
+  *
+  * @package energine
+  * @subpackage aux
+  * @author d.pavka@gmail.com
+  */
+ class RSSChannel extends DataSet {
+    /**
+     * Конструктор класса
+     *
+     * @param string $name
+     * @param string $module
+     * @param Document $document
+     * @param array $params
+     * @access public
+     */
+    public function __construct($name, $module, Document $document,  array $params = null) {
+        parent::__construct($name, $module, $document,  $params);
+        DocumentController::getInstance()->getTransformer()->setFileName('rss.xslt');
+        Response::getInstance()->setHeader('Content-Type', 'application/rss+xml; charset=UTF-8');
+    }
+}
---
+++ incubator/additional/transformers/rss.xslt
@@ -0,0 +1,13 @@
+<?xml version='1.0' encoding="UTF-8"?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+    <xsl:output method="xml"
+        version="1.0"
+        encoding="utf-8"
+        omit-xml-declaration="yes"
+        media-type="application/rss+xml"
+        indent="no" />
+
+    <xsl:template match="/">
+    </xsl:template>
+
+</xsl:stylesheet>

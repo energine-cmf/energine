e25cdbccd25454c6d3a2fc0f81f7c41c22c66d32
yourick <yourick.po@gmail.com>
2009-10-02 10:52:22 +0000

    git-svn-id: https://energine.googlecode.com/svn/trunk@275 acdd3125-bb50-0410-93b3-95ffde4b7c7d

--- incubator/additional/components/ResumeForm.class.php
+++ incubator/additional/components/ResumeForm.class.php
@@ -192,7 +192,7 @@ final class ResumeForm extends DataSet {
 
 		$mail = new Mail();
 		$mail->setFrom($this->getConfigValue('mail.from'));
-		$mail->addTo($this->getConfigValue('misc.vacancy_email'));
+		$mail->addTo($this->getConfigValue('mail.vacancy_email'));
 		$mail->setSubject($this->translate('TXT_SUBJ_NEW_RESUME'));
 		$mail->addReplyTo($data['resume_candidate_email'], $data['resume_candidate_name']);
 		$mail->setText($this->translate('TXT_BODY_NEW_RESUME'), $data);

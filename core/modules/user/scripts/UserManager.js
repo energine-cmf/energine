647e0c7423f0cefc39775f9051b5aead2d6d3d38
drPavka <d.pavka@gmail.com>
2009-11-06 15:08:26 +0000

    http://code.google.com/p/energine/issues/detail?id=21
    git-svn-id: https://energine.googlecode.com/svn/trunk@373 acdd3125-bb50-0410-93b3-95ffde4b7c7d

--- core/modules/user/scripts/UserManager.js
+++ core/modules/user/scripts/UserManager.js
@@ -4,6 +4,6 @@ var UserManager  = new Class({
     Extends: GridManager,
     activate: function(){
         
-        this.request(this.singleTemplatePath + this.grid.getSelectedRecordKey() + '/activate/', null, this.loadPage.pass(this.pageList.currentPage, this));
+        this.request(this.singlePath + this.grid.getSelectedRecordKey() + '/activate/', null, this.loadPage.pass(this.pageList.currentPage, this));
     }
 });

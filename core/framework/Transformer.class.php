8c5f94b21ab4de9daf502bb5fe5d77fe641de9ab
drPavka <d.pavka@gmail.com>
2010-03-17 11:27:16 +0000

    Добавлена возможность использовать xslcache http://code.nytimes.com/projects/xslcache
    git-svn-id: https://energine.googlecode.com/svn/trunk@679 acdd3125-bb50-0410-93b3-95ffde4b7c7d

--- core/framework/Transformer.class.php
+++ core/framework/Transformer.class.php
@@ -38,12 +38,12 @@ class Transformer extends Object {
 		parent::__construct();
 		$this->setFileName($this->getConfigValue('document.transformer'));
 	}
-    /**
-     * Устанавливает имя основного файла трансформации
-     * 
-     * @param string  
-     * @return void
-     */
+	/**
+	 * Устанавливает имя основного файла трансформации
+	 *
+	 * @param string
+	 * @return void
+	 */
 	public function setFileName($transformerFilename){
 		$transformerFilename = self::MAIN_TRANSFORMER_DIR.$transformerFilename;
 		if (!file_exists($transformerFilename)) {
@@ -61,16 +61,28 @@ class Transformer extends Object {
 	 * @access public
 	 */
 	public function transform($document, $transformerFilename = null) {
-		$xsltProc = new XSLTProcessor;
 		if (!is_null($transformerFilename)) {
 			$this->setFileName($transformerFilename);
 		}
-		$xsltDoc = new DOMDocument('1.0', 'UTF-8');
-		if (!@$xsltDoc->load($this->fileName)) {
-			throw new SystemException('ERR_DEV_NOT_WELL_FORMED_XSLT', SystemException::ERR_DEVELOPER);
+		//При наличии модуля xslcache http://code.nytimes.com/projects/xslcache
+		//используем его
+		if(extension_loaded('xslcache')){
+			$xsltProc = new xsltCache;
+			
+			//есть одна проблема с ним
+			//при неправильном xslt - сваливается в корку с 500 ошибкой
+		    $xsltProc->importStyleSheet($this->fileName);
+			$result =  $xsltProc->transformToXML($document);
 		}
-		$xsltProc->importStylesheet($xsltDoc);
-		$result = $xsltProc->transformToXml($document);
+		else {
+			$xsltProc = new XSLTProcessor;
+			$xsltDoc = new DOMDocument('1.0', 'UTF-8');
+			if (!@$xsltDoc->load($this->fileName)) {
+				throw new SystemException('ERR_DEV_NOT_WELL_FORMED_XSLT', SystemException::ERR_DEVELOPER);
+			}
+			$xsltProc->importStylesheet($xsltDoc);
+			$result = $xsltProc->transformToXml($document);
+		}
 		return $result;
 	}
 }

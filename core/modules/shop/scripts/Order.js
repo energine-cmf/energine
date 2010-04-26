857636e97896150caff76bcba0cafd46f94ebb8f
drPavka <d.pavka@gmail.com>
2010-04-26 20:51:04 +0000

    git-svn-id: https://energine.googlecode.com/svn/trunk@855 acdd3125-bb50-0410-93b3-95ffde4b7c7d

---
+++ core/modules/shop/scripts/Order.js
@@ -0,0 +1,12 @@
+ScriptLoader.load('Validator');
+
+var Order = new Class({
+	initialize: function(objID){
+		this.validator = new Validator(this.form = $(objID));
+	},
+	validate: function () {
+		if(this.validator.validate()){
+			this.form.submit();		
+		}
+	}
+});

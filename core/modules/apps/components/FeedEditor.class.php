d5a2c111cc0f02cba835a845437ae5716439ba6f
drPavka <d.pavka@gmail.com>
2010-06-29 09:58:10 +0000

    класс наследует от LinkingEditor'а
    git-svn-id: https://energine.googlecode.com/svn/trunk@932 acdd3125-bb50-0410-93b3-95ffde4b7c7d

--- core/modules/apps/components/FeedEditor.class.php
+++ core/modules/apps/components/FeedEditor.class.php
@@ -16,14 +16,7 @@
  * @subpackage share
  * @author dr.Pavka
  */
-class FeedEditor extends Grid {
-	/**
-	 * Включен ли режим редактировани
-	 *
-	 * @var boolean
-	 * @access private
-	 */
-	private $isEditable;
+class FeedEditor extends LinkingEditor {
 
 	/**
 	 * Конструктор класса
@@ -36,24 +29,6 @@ class FeedEditor extends Grid {
 	 */
 	public function __construct($name, $module, Document $document, array $params = null) {
 		parent::__construct($name, $module, $document, $params);
-		$this->isEditable = $this->document->isEditable();
-		$this->setProperty('exttype', 'feededitor');
-	}
-
-	/**
-	 * Добавляем параметр  - имя связанного компонента
-	 *
-	 * @return array
-	 * @access protected
-	 */
-
-	protected function defineParams() {
-		return array_merge(
-		parent::defineParams(),
-		array(
-        'linkTo' => false
-		)
-		);
 	}
 
 	/**
@@ -93,67 +68,6 @@ class FeedEditor extends Grid {
 		}
 		return $result;
 	}
-
-	/**
-	 * Убираем все лишнее
-	 *
-	 * @return void
-	 * @access protected
-	 */
-
-	protected function main() {
-		$_SESSION['feed_smap_id'] =
-		$this->document->componentManager->getComponentByName(
-		$this->getParam('linkTo')
-		)->getFilter();
-		$this->addToolbar($this->createToolbar());
-		$this->js = $this->buildJS();
-	}
-
-	protected function changeOrder($direction){
-		$this->setFilter($_SESSION['feed_smap_id']);
-		//unset($_SESSION['feed_smap_id']);
-
-		return parent::changeOrder($direction);
-	}
-
-	/**
-	 * Для метода main убираем вызов построителя
-	 *
-	 * @return DOMDocument
-	 * @access public
-	 */
-
-	public function build() {
-		if ($this->getAction() == 'main') {
-			if ($param = $this->getParam('linkTo')) {
-				$this->setProperty('linkedComponent', $param);
-			}
-			$result = Component::build();
-			if (($component = $this->document->componentManager->getComponentByName($param)) && ($component->getAction() != 'view') && $this->isEditable) {
-				if ($this->js) {
-					$result->documentElement->appendChild($result->importNode($this->js, true));
-				}
-				$result->documentElement->appendChild($result->createElement('recordset'));
-				if (($tbs = $this->getToolbar()) && (!empty($tbs))) {
-				    foreach($tbs as $tb)
-					if($toolbar = $tb->build()) {
-					    $result->documentElement->appendChild($result->importNode($toolbar, true));
-					}
-				}
-				 
-			}
-		}
-		else {
-			if ($this->getType() !== self::COMPONENT_TYPE_LIST ) {
-				$this->setProperty('exttype', 'grid');
-			}
-			$result = parent::build();
-		}
-
-		return $result;
-	}
-
 
 	/**
 	 * Выставляем smap_id в текущее значение

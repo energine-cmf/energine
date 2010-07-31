3c003a589ef8df650f3d608751ee2cadc0b463aa
drPavka <d.pavka@gmail.com>
2010-07-31 12:43:10 +0000

    Удалены неиспользумые переменные
    git-svn-id: https://energine.googlecode.com/svn/trunk@993 acdd3125-bb50-0410-93b3-95ffde4b7c7d

--- core/framework/DBStructureInfo.class.php
+++ core/framework/DBStructureInfo.class.php
@@ -33,6 +33,7 @@ final class DBStructureInfo extends Object {
      * @var PDO
      */
     private $pdo;
+
     /**
      *
      * @param PDO $pdo
@@ -42,18 +43,20 @@ final class DBStructureInfo extends Object {
         parent::__construct();
         $this->pdo = $pdo;
 
-        if($this->getConfigValue('cache.enable')) {
+        if ($this->getConfigValue('cache.enable')) {
             $this->memcache = new Memcached();
             $this->memcache->addServer($this->getConfigValue('cache.host'), $this->getConfigValue('cache.port'));
-            if(!($str = $this->memcache->get('structure'))) {
-                $this->memcache->set('structure', serialize($this->structure = $this->collectDBInfo()));
+            if (!($str = $this->memcache->get('structure'))) {
+                $this->memcache->set('structure', serialize(
+                    $this->structure = $this->collectDBInfo()));
             }
             else {
                 $this->structure = unserialize($str);
-                //$this->memcache->delete('structure');
             }
+            //$this->memcache->delete('structure');
         }
     }
+
     /**
      * Собирает информацию о структуре всех таблиц в БД
      * вызывается только при использовании кеша
@@ -63,7 +66,7 @@ final class DBStructureInfo extends Object {
      */
     private function collectDBInfo() {
         $res = $this->pdo->query('SHOW TABLES');
-        if($res) {
+        if ($res) {
             while ($tableName = $res->fetchColumn()) {
                 $result[$tableName] = $this->getTableMeta($tableName);
             }
@@ -71,6 +74,7 @@ final class DBStructureInfo extends Object {
 
         return $result;
     }
+
     /**
      * Проверка таблицы на существование
      *
@@ -80,9 +84,10 @@ final class DBStructureInfo extends Object {
     public function tableExists($tableName) {
         $result = false;
         //Если не существует в кеше
-        if(!isset($this->structure[$tableName])) {
+        if (!isset($this->structure[$tableName])) {
             //если существует в списке таблиц
-            if($this->pdo->query('SHOW TABLES LIKE \''.$tableName.'\'')->rowCount()) {
+            if ($this->pdo->query(
+                'SHOW TABLES LIKE \'' . $tableName . '\'')->rowCount()) {
                 $result = true;
                 $this->structure[$tableName] = array();
             }
@@ -96,6 +101,7 @@ final class DBStructureInfo extends Object {
         }
         return $result;
     }
+
     /**
      * Возвращает информацию о колонках таблицы
      * Записывает данные в $this->structure для последующего использования
@@ -103,18 +109,18 @@ final class DBStructureInfo extends Object {
      * @param  string $tableName
      * @return array|mixed
      */
-    public function getTableMeta($tableName) {
-        //inspect($this->analyzeCreateTableSQL($this->pdo->query("SHOW CREATE TABLE `$tableName`")->fetchColumn(1)));
-        
-		if(!isset($this->structure[$tableName]) || ($this->structure[$tableName] === array())){
-			$res = $this->pdo->query("SHOW COLUMNS FROM `$tableName`");
-            if($res) {
+ /*   public function getTableMeta($tableName) {
+        if (!isset($this->structure[$tableName]) ||
+                ($this->structure[$tableName] === array())) {
+            $res = $this->pdo->query("SHOW COLUMNS FROM `$tableName`");
+            if ($res) {
                 $this->structure[$tableName] = array();
-                while($row = $res->fetch(PDO::FETCH_ASSOC)){
+                while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
                     $name = $row['Field'];
                     //$type = strtoupper(strstr($row['Type'], '(', true));
 
-                    $nullable = (strtolower($row['Null']) == 'yes' ? true : false);
+                    $nullable =
+                            (strtolower($row['Null']) == 'yes' ? true : false);
                     $index = $row['Key'];
                     $length = false;
 
@@ -126,71 +132,124 @@ final class DBStructureInfo extends Object {
                         }
                     }
                     $type = self::convertType($type);
-                    
+
                     switch ($index) {
-					case 'PRI':
-						$fk = $this->getForeignKeyInfo($tableName, $name);
-						$key = ($fk == false ? true : $fk);
-						break;
-					case 'MUL':
-						$key = $this->getForeignKeyInfo($tableName, $name);
-						break;
-					default:
-						$key = false;
-				    }
-                    $default = (empty($row['Default']))?false:$row['Default'];
-                    $this->structure[$tableName][$name] = compact( 'nullable', 'length', 'default', 'key', 'type' , 'tableName', 'index');
+                        case 'PRI':
+                            $fk = $this->getForeignKeyInfo($tableName, $name);
+                            $key = ($fk == false ? true : $fk);
+                            break;
+                        case 'MUL':
+                            $key = $this->getForeignKeyInfo($tableName, $name);
+                            break;
+                        default:
+                            $key = false;
+                    }
+                    $default =
+                            (empty($row['Default'])) ? false : $row['Default'];
+                    $this->structure[$tableName][$name] =
+                            compact('nullable', 'length', 'default', 'key', 'type', 'tableName', 'index');
                 }
             }
-		}
-
+        }
+inspect($tableName, $this->structure[$tableName]);
         return $this->structure[$tableName];
     }
-    
-    private function analyzeCreateTableSQL($sql){
+*/
+    public function getTableMeta($tableName) {
+        if (!isset($this->structure[$tableName]) ||
+                ($this->structure[$tableName] === array())) {
+            $this->structure[$tableName] = $this->analyzeCreateTableSQL($this->pdo->query("SHOW CREATE TABLE `$tableName`")->fetchColumn(1));
+            $this->structure[$tableName] = array_map(function($row) use($tableName){ $row['tableName'] = $tableName; return $row;}, $this->structure[$tableName]);
+            //inspect($tableName, $this->structure[$tableName]);
+        }
+        return $this->structure[$tableName];
+    }
+
+
+    private function analyzeCreateTableSQL($sql) {
         $res = array();
         $s = strpos($sql, '(');
         $l = strrpos($sql, ')') - $s;
-        $fields = substr($sql, $s+1, $l);
-        $fields = str_replace("\n", '', $fields);
-        $fields = explode(',', $fields);
 
-        $trimQ = function($s) { return trim($s, '`'); };
+        // работаем только с полями и индексами
+        $fields = substr($sql, $s + 1, $l);
 
-        $pattern = '/PRIMARY KEY \(([^\)]*)\)/';
-        if(preg_match($pattern, $sql, $matches)){
-            $res['pri'] = array_map($trimQ, explode(',', $matches[1]));
+        $trimQ = function($s) {
+            return trim($s, '`');
+        };
+
+        $row = '(?:^\s*`(?<name>\w+)`' . // field name
+                '\s+(?<type>\w+)' . //field type
+                '(?:\((?<len>\w+)\))?' . //field len
+                '(?:\s+(?<unsigned>unsigned))?' .
+                '(?:\s*(?<is_null>(?:NOT )?NULL))?' .
+                '(?:\s+DEFAULT (?<default>(?:NULL|\'[^\']+\')))?' .
+                '.*$)';
+        $constraint =
+                '(?:^\s*CONSTRAINT `\w+` FOREIGN KEY \(`(?<cname>\w+)`\) REFERENCES `(?<tableName>\w+)` \(`(?<fieldName>\w+)`\)' .
+                        '.*$)';
+        $mul = '(?:^\s*(?:UNIQUE\s*)?KEY\s+`\w+`\s+\((?<muls>.*)\),?$)';
+
+        $pri = '(?:PRIMARY KEY \((?<pri>[^\)]*)\))';
+
+        $pattern = "/(?:$row|$constraint|$mul|$pri)/im";
+
+        if (preg_match_all($pattern, trim($fields), $matches)) {
+            if ($matches['name']) {
+                // список полей в первичном ключе
+                $pri = array();
+                if (isset($matches['pri'])) {
+                    foreach ($matches['pri'] as $priField) {
+                        if ($priField) {
+                            $pri = array_map($trimQ, explode(',', $priField));
+                            break;
+                        }
+                    }
+                }
+
+                // список полей входящих в индексы
+                $muls = array();
+                if (isset($matches['muls'])) {
+                    $mulStr = '';
+                    foreach ($matches['muls'] as $s) {
+                        if ($s) $mulStr .= ($mulStr ? ',' : '') . $s;
+                    }
+                    $muls = array_map($trimQ, explode(',', $mulStr));
+                }
+
+                foreach ($matches['name'] as $index => $fieldName) {
+                    if (!$fieldName) continue;
+                    $res[$fieldName] = array(
+                        'type' => self::convertType($matches['type'][$index]),
+                        'length' => (int)$matches['len'][$index],
+                        'nullable' => (
+                                $matches['is_null'][$index] != 'NOT NULL'),
+                        'default' => trim($matches['default'][$index], "'"),
+                        'key' => false,
+                        'index' => false
+                    );
+
+                    // входит ли поле в индекс
+                    if (in_array($fieldName, $pri)) {
+                        $res[$fieldName]['index'] = 'PRI';
+                        $res[$fieldName]['key'] = true;
+                    }
+                    elseif (in_array($fieldName, $muls)) {
+                        $res[$fieldName]['index'] = 'MUL';
+                    }
+
+                    // внешний ключ
+                    $cIndex = array_search($fieldName, $matches['cname']);
+                    if ($cIndex !== false) {
+                        $res[$fieldName]['key'] = array(
+                            'tableName' => $matches['tableName'][$cIndex],
+                            'fieldName' => $matches['fieldName'][$cIndex],
+                        );
+                    }
+                }
+            }
         }
 
-        foreach($fields as $field){
-            $pattern = '/^`(?<name>\w+)`'. // field name
-                    '\s+(?<type>\w+)'.  //field type
-                    '(?:\((?<len>\w+)\))?'. //field len
-                    '(?:\s+(?<unsigned>unsigned))?'.
-                    '(?:\s*(?<is_null>(?:NOT )?NULL))?'.
-                    '(?:\s+DEFAULT (?<default>(?:NULL|\'[^\']+\')))?'.
-                    '.*/i';
-
-            if(preg_match($pattern, trim($field), $matches)){
-                $res[$matches['name']] = $matches;
-                continue;
-            }
-            $pattern = '/CONSTRAINT `\w+` FOREIGN KEY \(`(?<name>\w+)`\) REFERENCES `(?<table>\w+)` \(`(?<fieldName>\w+)`\).*/';
-            if(preg_match($pattern, trim($field), $matches)){
-                if(!array_key_exists('key', $res))$res['key'] = array();
-                $res['key'][$matches['name']] = $matches;
-                continue;
-            }
-
-            $pattern = '/^KEY\s+`\w+`\s+\(([^\)]*)\)/';
-            if(preg_match($pattern, trim($field), $matches)){
-                if(!array_key_exists('mul', $res)) $res['mul'] = array();
-                $res['mul'] = array_merge(
-                    $res['mul'],
-                    array_map($trimQ, explode(',', $matches[1]))
-                );
-            }
-        }
         return $res;
     }
 
@@ -204,24 +263,28 @@ final class DBStructureInfo extends Object {
      * @staticvar array $createTableInfo
      */
     private function getForeignKeyInfo($tableName, $fieldName) {
-		static $foreignKeyInfo, $createTableInfo;
-        if(!isset($createTableInfo[$tableName])){
-            $createTableInfo[$tableName] = $this->pdo->query("SHOW CREATE TABLE `$tableName`")->fetchColumn(1);
+        static $foreignKeyInfo, $createTableInfo;
+        if (!isset($createTableInfo[$tableName])) {
+            $createTableInfo[$tableName] =
+                    $this->pdo->query("SHOW CREATE TABLE `$tableName`")->fetchColumn(1);
         }
-		if(!isset($foreignKeyInfo[$tableName][$fieldName])){
-			$res = preg_match_all("/FOREIGN KEY \(`([_a-z0-9]+)`\) REFERENCES `([^`]+)` \(`([^`]+)`\)/m", $createTableInfo[$tableName], $matches, PREG_SET_ORDER);
-			if(!empty($res)){
-				foreach($matches as $row){
-					$foreignKeyInfo[$tableName][$row[1]] = array('tableName' => $row[2], 'fieldName' => $row[3]);
-				}
-			}
-		}
-		if(!isset($foreignKeyInfo[$tableName][$fieldName])){
-			$foreignKeyInfo[$tableName][$fieldName] = false;
-		}
-        
-		return $foreignKeyInfo[$tableName][$fieldName];
-	}
+        if (!isset($foreignKeyInfo[$tableName][$fieldName])) {
+            $res =
+                    preg_match_all("/FOREIGN KEY \(`([_a-z0-9]+)`\) REFERENCES `([^`]+)` \(`([^`]+)`\)/m", $createTableInfo[$tableName], $matches, PREG_SET_ORDER);
+            if (!empty($res)) {
+                foreach ($matches as $row) {
+                    $foreignKeyInfo[$tableName][$row[1]] =
+                            array('tableName' => $row[2], 'fieldName' => $row[3]);
+                }
+            }
+        }
+        if (!isset($foreignKeyInfo[$tableName][$fieldName])) {
+            $foreignKeyInfo[$tableName][$fieldName] = false;
+        }
+
+        return $foreignKeyInfo[$tableName][$fieldName];
+    }
+
     /**
      * Конвертирует MYSQL типы полей в Energine типы полей
      *
@@ -230,51 +293,51 @@ final class DBStructureInfo extends Object {
      * @return string
      */
     static private function convertType($mysqlType) {
-		$result = $mysqlType;
-		switch ($mysqlType) {
-			case 'TINYINT':
-			case 'MEDIUM':
-			case 'SMALLINT':
-			case 'INT':
-			case 'BIGINT':
-				$result = DBA::COLTYPE_INTEGER;
-				break;
-			case 'FLOAT':
-			case 'DOUBLE':
-			case 'DECIMAL':
-			case 'NUMERIC':
-				$result = DBA::COLTYPE_FLOAT;
-				break;
-			case 'DATE':
-				$result = DBA::COLTYPE_DATE;
-				break;
-			case 'TIME':
-				$result = DBA::COLTYPE_TIME;
-				break;
-			case 'TIMESTAMP':
-				$result = DBA::COLTYPE_TIMESTAMP;
-				break;
-			case 'DATETIME':
-				$result = DBA::COLTYPE_DATETIME;
-				break;
-			case 'VARCHAR':
-			case 'CHAR':
-				$result = DBA::COLTYPE_STRING;
-				break;
-			case 'TEXT':
-			case 'TINYTEXT':
-			case 'MEDIUMTEXT':
-			case 'LONGTEXT':
-				$result = DBA::COLTYPE_TEXT;
-				break;
-			case 'BLOB':
-			case 'TINYBLOB':
-			case 'MEDIUMBLOB':
-			case 'LONGBLOB':
-				$result = DBA::COLTYPE_BLOB;
-				break;
-			default: // не используется
-		}
-		return $result;
-	}
+        $result = $mysqlType = strtoupper($mysqlType);
+        switch ($mysqlType) {
+            case 'TINYINT':
+            case 'MEDIUM':
+            case 'SMALLINT':
+            case 'INT':
+            case 'BIGINT':
+                $result = DBA::COLTYPE_INTEGER;
+                break;
+            case 'FLOAT':
+            case 'DOUBLE':
+            case 'DECIMAL':
+            case 'NUMERIC':
+                $result = DBA::COLTYPE_FLOAT;
+                break;
+            case 'DATE':
+                $result = DBA::COLTYPE_DATE;
+                break;
+            case 'TIME':
+                $result = DBA::COLTYPE_TIME;
+                break;
+            case 'TIMESTAMP':
+                $result = DBA::COLTYPE_TIMESTAMP;
+                break;
+            case 'DATETIME':
+                $result = DBA::COLTYPE_DATETIME;
+                break;
+            case 'VARCHAR':
+            case 'CHAR':
+                $result = DBA::COLTYPE_STRING;
+                break;
+            case 'TEXT':
+            case 'TINYTEXT':
+            case 'MEDIUMTEXT':
+            case 'LONGTEXT':
+                $result = DBA::COLTYPE_TEXT;
+                break;
+            case 'BLOB':
+            case 'TINYBLOB':
+            case 'MEDIUMBLOB':
+            case 'LONGBLOB':
+                $result = DBA::COLTYPE_BLOB;
+                break;
+            default: // не используется
+        }
+        return $result;
+    }
 }

ed3bc56e38e665fb3b931fb4cda3d2b982ddf405
sergey.sign <sergey.sign>
2010-08-06 14:01:12 +0000

    comments: add class CommentsList
    git-svn-id: https://energine.googlecode.com/svn/trunk@1002 acdd3125-bb50-0410-93b3-95ffde4b7c7d

---
+++ core/modules/comments/components/CommentsList.class.php
@@ -0,0 +1,214 @@
+<?php
+/**
+ * Содержит класс CommentsList
+ *
+ * @package energine
+ * @subpackage comments
+ * @author sign
+ */
+
+/**
+ * Компонент комментариев
+ *
+ * @package energine
+ * @subpackage comments
+ * @author sign
+ *
+ * Пример использования
+ *
+ * $commentsParams = array(
+            'table_name' => $this->getTableName(),
+            'is_tree' => false,
+            'target_ids' => $this->getData()->getFieldByName('news_id')->getData()
+        );
+        $commentsList = $this->document->componentManager->createComponent('commentsList', 'comments', 'CommentsList', $commentsParams);
+        $commentsList->run();
+        $this->document->componentManager->addComponent($commentsList);
+ */
+class CommentsList extends DataSet
+{
+	/**
+	 * Коментарии древовидные
+	 * @var bool
+	 */
+	protected $isTree = false;
+
+
+    protected $targetIds = array();
+
+    /**
+     * @var array
+     */
+    private $loadedData = null;
+
+    /**
+     * @var CommentsHelper
+     */
+    protected $comments = null;
+
+    /**
+     * Конструктор
+     *
+     * В $param ожидаются поля
+     * table_name string - имя комментируемой таблицы
+     * target_ids array - айдишники комментируемых сущностей
+     * is_tree bool - комментарии древовидные?
+     *
+     * @param string  $name
+     * @param string  $module
+     * @param Document $document
+     * @param  array $params
+     * @return void
+     */
+    public function __construct($name, $module, Document $document,  array $params = null) {
+        parent::__construct($name, $module, $document,  $params);
+        $this->setProperty('exttype', 'comments');
+        $this->setProperty('type', 'list');
+
+        $commentTable = DocumentController::getInstance()->dbh->tableExists($this->getParam('table_name'). '_comment');
+
+		$this->isTree = $this->getParam('is_tree');
+        $this->targetIds = $this->getParam('target_ids');
+
+        $this->comments = new Comments($commentTable, $this->isTree);
+    }
+
+    /**
+     * Описание полей комментария
+     * @return DataDescription
+     */
+    protected function createDataDescription(){
+        $dataDescription = new DataDescription();
+
+		$fd = new FieldDescription('comment_id');
+		$fd->setType(FieldDescription::FIELD_TYPE_INT);
+		$fd->setProperty('key', true); // для построения дерева
+		$dataDescription->addFieldDescription($fd);
+
+		// если у нас древовидная структура - добавляем предка
+		if($this->isTree){
+			$fd = new FieldDescription('comment_parent_id');
+			$fd->setType(FieldDescription::FIELD_TYPE_INT);
+			$dataDescription->addFieldDescription($fd);
+		}
+
+		// комментиркемая сущность
+		$fd = new FieldDescription('target_id');
+		$fd->setType(FieldDescription::FIELD_TYPE_INT);
+		$dataDescription->addFieldDescription($fd);
+
+		$fd = new FieldDescription('u_id');
+		$fd->setType(FieldDescription::FIELD_TYPE_INT);
+		$dataDescription->addFieldDescription($fd);
+
+		$fd = new FieldDescription('comment_created');
+		$fd->setType(FieldDescription::FIELD_TYPE_DATETIME);
+		$dataDescription->addFieldDescription($fd);
+
+		$fd = new FieldDescription('comment_name');
+		$fd->setType(FieldDescription::FIELD_TYPE_STRING);
+		$dataDescription->addFieldDescription($fd);
+
+		$fd = new FieldDescription('comment_approved');
+		$fd->setType(FieldDescription::FIELD_TYPE_BOOL);
+		$dataDescription->addFieldDescription($fd);
+
+		// Инфа о юзере
+		$fd = new FieldDescription('u_fullname');
+		$fd->setType(FieldDescription::FIELD_TYPE_STRING);
+		$dataDescription->addFieldDescription($fd);
+
+		$fd = new FieldDescription('u_avatar_img');
+		$fd->setType(FieldDescription::FIELD_TYPE_IMAGE);
+		$dataDescription->addFieldDescription($fd);
+
+		return $dataDescription;
+    }
+
+    /**
+     * Создать билдер
+     *
+     * Если комментарии не древовидные или отсутствуют возвращается Builder иначе - TreeBuilder 
+     * @return Builder|TreeBuilder
+     */
+    protected function createBuilder(){
+        return $this->isTree && !$this->loadData()? new TreeBuilder() : new Builder();
+    }
+
+    /**
+     * Загружаем комментарии и информацию о пользователях
+     *
+     * @return array
+     */
+    protected function loadData(){
+        if(is_null($this->loadedData)){
+            $this->loadedData =  $this->comments->getListByIds($this->targetIds);
+            $this->loadedData = $this->addUsersInfo($this->loadedData);
+        }
+        return $this->loadedData;
+    }
+
+
+    /**
+     * @return array
+     */
+    protected function defineParams() {
+        return array_merge(
+            parent::defineParams(),
+            array(
+                'table_name' => '',
+                'is_tree' => false,
+                'target_ids' => array(),
+            )
+        );
+    }
+
+    /**
+	 * Внедряем инфу о юзерах в комментарии
+	 *
+	 * @param array $data
+	 * @return array
+	 */
+	private function addUsersInfo($data){
+		if($data && is_array($data)){
+			$usersInfo = $this->getUsersByComments($data);
+			$usersInfo = convertDBResult($usersInfo, 'u_id');
+
+			foreach($data as &$item){
+				$user = $usersInfo[$item['u_id']];
+				$item['u_fullname'] = $user['u_fullname'];
+				$item['u_avatar_img'] = $user['u_avatar_img'];
+			}
+		}
+		return $data;
+	}
+
+	/**
+	 * Информация о юзерах оставивших комментарии
+	 *
+	 * @param array $data
+	 * @return array
+	 */
+	private function getUsersByComments($data){
+		$result = array();
+
+		if($data && is_array($data)){
+			$userIds =  array();
+			foreach($data as $item){
+				$userIds[] = $item['u_id'];
+			}
+			$userIds = array_unique($userIds);
+
+			if($userIds){
+				$userIds = implode(',', $userIds);
+				$result = $this->dbh->selectRequest(
+					"SELECT *
+					 FROM user_users
+					 WHERE u_id in($userIds)"
+				);
+			}
+		}
+		return $result;
+	}
+}
+

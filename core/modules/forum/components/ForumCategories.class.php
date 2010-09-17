4658763e5a5d160ba8d75018f2641120a313cd23
drPavka <d.pavka@gmail.com>
2010-09-17 16:32:31 +0000

    git-svn-id: https://energine.googlecode.com/svn/trunk@1084 acdd3125-bb50-0410-93b3-95ffde4b7c7d

--- core/modules/forum/components/ForumCategories.class.php
+++ core/modules/forum/components/ForumCategories.class.php
@@ -46,18 +46,25 @@ class ForumCategories extends PageList {
         return $result;
     }
 
-    /*protected function loadData() {
+    protected function loadData() {
         $result = parent::loadData();
         foreach($result as $smapID => $smapInfo){
             $categoryInfo = $this->dbh->selectRequest('
-                SELECT COUNT(ft.theme_id) as theme_count, SUM(comment_num) as comment_count, ft.comment_id '.
+                SELECT COUNT(ft.theme_id) as theme_count, SUM(comment_num) as comment_count,'.
+                    ' CASE WHEN ft.comment_id IS NULL THEN ut.u_nick ELSE uc.u_nick END as nick,'.
+                    ' CASE WHEN ft.comment_id IS NULL THEN ft.theme_created ELSE ftc.comment_created END as comment_created'.
                     ' FROM forum_theme ft'.
-                    ' LEFT JOIN forum_theme_comment ftc ON ft.comment_id=ftc.comment_id'.
+                    ' LEFT JOIN forum_theme_comment ftc ON ft.comment_id=ftc.comment_id '.
+                    ' LEFT JOIN user_users uc ON uc.u_id = ftc.u_id '.
+                    ' LEFT JOIN user_users ut ON ut.u_id = ft.u_id '.
                     ' WHERE smap_id = %s', $smapID);
-inspect($categoryInfo);
-            //$result[$smapID]['ThemeCount'] = simplifyDBResult(, 'theme',true);
+
+            $result[$smapID]['ThemeCount'] = simplifyDBResult($categoryInfo, 'theme_count',true);
+            $result[$smapID]['CommentCount'] = simplifyDBResult($categoryInfo, 'comment_count',true);
+            $result[$smapID]['CommentCreated'] = simplifyDBResult($categoryInfo, 'comment_created',true);
+            $result[$smapID]['Nick'] = simplifyDBResult($categoryInfo, 'nick',true);
         }
 //inspect($result);
         return $result;
-    }*/
+    }
 }

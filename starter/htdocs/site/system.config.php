f9452774ce78caf42e460b34988972d47e93fef6
drPavka <d.pavka@gmail.com>
2011-01-14 13:17:18 +0000

    git-svn-id: https://energine.googlecode.com/svn/branches/2.6@1421 acdd3125-bb50-0410-93b3-95ffde4b7c7d

--- starter/htdocs/site/system.config.php
+++ starter/htdocs/site/system.config.php
@@ -4,8 +4,8 @@ return array(
     'database' => array(
         'master' => array(
             'dsn' => 'host=localhost;port=3306;dbname=energine-2.6',
-            'username' => 'root',
-            'password' => 'qwerqaz123'
+            'username' => '',
+            'password' => ''
         )
     ),
     'site' => array(
@@ -30,43 +30,9 @@ return array(
         'lifespan' => 108000,
     ),
     'mail' => array(
-        'from' => 'noreply@stb.ua',
-        'manager' => 'd.pavka@gmail.com',
-        'feedback' => 'd.pavka@gmail.com,zablotsky.a@stb.ua'
-    ),
-    'thumbnails' => array(
-        'middle' => array(
-            'resize_rule' => 'append',
-            'width' => 298,
-            'height' => 224,
-            'logo' => '/srv/media/origin/logo.png',
-            'gag' => 'images/no_photo_m.gif'
-        ),
-        'small' => array(
-            'resize_rule' => 'append',
-            'width' => 90,
-            'height' => 68,
-            'logo' => '/srv/media/origin/logo.png',
-            'gag' => 'images/no_photo_s.gif'
-        ),
-        'xxsmall' => array(
-            'resize_rule' => 'append',
-            'width' => 50,
-            'height' => 50,
-            'logo' => '/srv/media/origin/logo.png',
-            'gag' => 'images/no_photo_xs.gif'
-        ),
-        'middle cut' => array(
-            'resize_rule' => 'crop',
-            'width' => 298,
-            'height' => 140,
-            'logo' => '/srv/media/origin/logo_50.png',
-            'gag' => 'images/no_photo_mc.gif'
-        ),
-    ),
-    'sphinx' => array(
-        'ql_host' => 'localhost',
-        'ql_port' => 3307
+        'from' => 'noreply@energine.org',
+        'manager' => 'demo@energine.org',
+        'feedback' => 'demo@energine.org'
     ),
     'video' => array(
         'ffmpeg' => '/usr/bin/ffmpeg'

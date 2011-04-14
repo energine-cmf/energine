f70d3e8492bf8532d2a3c66d4f9a443e4bfce2f9
spacelord.5 <spacelord.5>
2011-04-14 12:38:28 +0000

    Змінено роботу зі smap_id.
    git-svn-id: https://energine.googlecode.com/svn/branches/2.6@1557 acdd3125-bb50-0410-93b3-95ffde4b7c7d

--- core/modules/apps/components/Feed.class.php
+++ core/modules/apps/components/Feed.class.php
@@ -61,22 +61,57 @@ class Feed extends DBDataSet
             }
         }
     }
+    /*
+     * @access protected
+     * @return DataDescription
+     * Якщо у конфігі є smap_id, то змінюємо його тип на int.
+     * Потрібно для того, щоб при проходжені подальшого циклу ми не лізли в таблицю share_sitemap.
+     * Натомість smap_id у нас реально буде містити тільки smap_id as integer
+     * */
+    protected function loadDataDescription(){
+        $result = parent::loadDataDescription();
+        if(isset($result['smap_id'])){
+            $result['smap_id']['key'] = false;
+        }
+        return $result;
+    }
 
     /**
      * Убираем smap_id
      *
      * @access protected
      * @return DataDescription
+     * Якщо у конфігі не вказано smap_id, створюємо його, вказуємо таблицю з якою брати значення й додаємо до DataDescription.
+     * Потім змінюємо тип smap_id на FIELD_TYPE_HIDDEN.
+     * 
      */
     protected function createDataDescription()
     {
         $result = parent::createDataDescription();
-        /*if ($smapField = $result->getFieldDescriptionByName('smap_id')) {
-            $result->removeFieldDescription($smapField);
-        }*/
-
+        if(!($fd = $result->getFieldDescriptionByName('smap_id'))){
+            $fd = new FieldDescription('smap_id');
+            $fd->setProperty('tableName',$this->getTableName());
+            $result->addFieldDescription($fd);
+        }
+        $fd->setType(FieldDescription::FIELD_TYPE_HIDDEN);
         return $result;
     }
+    /*
+     * Окрім батьківського методу ми також робимо деякі дії зі smap_id,
+     * а саме записуємо йому властивістю url повне значення smap_segment.
+     * Таким чином, якщо у нас на одній сторінці Feed витягне елементи з різними smap_id,
+     * то ми завжди будемо знати як правильно сформувати link для того щоб дістатися до конкретного елементу.
+     *  
+     * */
+    protected function main(){
+        parent::main();
+        if($f = $this->getData()->getFieldByName('smap_id')){
+            foreach($f as $key=>$value){
+                $f->setRowProperty($key,'url',E()->getMap()->getURLByID($value));
+            }
+        }
+    }
+
 
     /**
      * Добавляем крошку

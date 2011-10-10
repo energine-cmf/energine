6e3f06ca2674b0d5a66ac6f382e017368f3de3f2
YNnoer@gmail.com <YNnoer@gmail.com>
2011-10-10 10:16:30 +0000

    git-svn-id: https://energine.googlecode.com/svn/trunk@1840 acdd3125-bb50-0410-93b3-95ffde4b7c7d

--- core/modules/apps/components/NewsEditor.class.php
+++ core/modules/apps/components/NewsEditor.class.php
@@ -114,7 +114,7 @@ class NewsEditor extends FeedEditor {
         // NOER: change data desc for field news_status to bool
         $news_status = $result->getFieldDescriptionByName('news_status');
         $news_status->setType(FieldDescription::FIELD_TYPE_BOOL);
-        // END NOER 
+        // END NOER (svn test)
 
         return $result;
 

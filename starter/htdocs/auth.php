723f1c884cddf67039cdb5108138a86fb51919b6
drPavka <d.pavka@gmail.com>
2011-12-12 15:27:39 +0000

    фейсбук авторизация
    git-svn-id: https://energine.googlecode.com/svn/trunk@1895 acdd3125-bb50-0410-93b3-95ffde4b7c7d

--- starter/htdocs/auth.php
+++ starter/htdocs/auth.php
@@ -17,6 +17,9 @@ if (
         isset($_POST['user']['password'])))
         ||
         ($logout = (isset($_POST['user']['logout']) || isset($_GET['logout'])))
+        ||
+        //($fbLogin = ((isset($_POST['fbData']))?$_POST['fbData']:false))
+        ($fbAuth = isset($_GET['fbAuth']))
 ) {
     $response = E()->getResponse();
 
@@ -45,6 +48,46 @@ if (
         //просто удаляем куку
         $response->deleteCookie(UserSession::DEFAULT_SESSION_NAME);
     }
-}
+    elseif(
+        $fbAuth
+        &&
+        ($appID = Object::_getConfigValue('auth.facebook.appID'))
+        &&
+        ($secretKey = Object::_getConfigValue('auth.facebook.secretKey'))
+    ){
+        require_once(str_replace('*', 'user', CORE_GEARS_DIR).'/facebook.php');
+        $FBL = new Facebook(array(
+          'appId'  => $appID,
+          'secret' => $secretKey,
+        ));
+        if($fbUID = $FBL->getUser()){
+            //Смотрим есть ли такой юзер в списке зарегистрированных
+            if(!($user = User::getFBUser($fbUID))){
+            //Если нет - создаем
+                try {
+                    //Обращаемся за данными
+                    $FBUserData = $FBL->api('/me');
+                    $userData = array(
+                        'u_name' =>$FBUserData['email'],
+                        'u_fbid' =>$FBUserData['id'],
+                        'u_password' => User::generatePassword(),
+                        'u_fullname' =>$FBUserData['name']
+                    );
+                    $user = new User();
+                    $user->create($userData);
+                }
+                catch(FacebookApiException $e){
+                    $response->addCookie(UserSession::FAILED_LOGIN_COOKIE_NAME, 'bad auth data', time()+60);
+                    goto escape;
+                }
+            }
+            call_user_func_array(
+                array($response, 'addCookie'),
+                UserSession::manuallyCreateSessionInfo($user->getID())
+            );
 
+        }
+    }
+}
+escape:
 $response->goBack();

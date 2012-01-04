9b600b8d1f99d0ba6213e7f5ea2cc2b89fe2980a
drPavka <d.pavka@gmail.com>
2012-01-04 13:21:34 +0000

    Брендирование(редактор)
    git-svn-id: https://energine.googlecode.com/svn/trunk@1941 acdd3125-bb50-0410-93b3-95ffde4b7c7d

---
+++ core/modules/apps/components/Branding.class.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Содержит класс Branding
+ *
+ * @package energine
+ * @subpackage apps
+ * @author dr.Pavka
+ * @copyright Energine 2012
+ */
+
+/**
+ * Вывод брендирования
+ *
+ * @package energine
+ * @subpackage apps
+ * @author dr.Pavka
+ */
+class Branding extends DBDataSet {
+
+}

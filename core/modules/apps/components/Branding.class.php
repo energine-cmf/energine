b77975c1f48eb2de044ce1d689ec2780e1b66832
drPavka <d.pavka@gmail.com>
2012-01-04 15:02:09 +0000

    git-svn-id: https://energine.googlecode.com/svn/trunk@1942 acdd3125-bb50-0410-93b3-95ffde4b7c7d

--- core/modules/apps/components/Branding.class.php
+++ core/modules/apps/components/Branding.class.php
@@ -16,5 +16,40 @@
  * @author dr.Pavka
  */
 class Branding extends DBDataSet {
+    public function __construct($name, $module, array $params = null) {
+        $params['active'] = false;
 
+        parent::__construct($name, $module, $params);
+        $this->setTableName('apps_branding');
+        $this->setFilter(array('brand_id' => $this->findNearestParentBrandID()));
+        $this->setParam('recordsPerPage', false);
+    }
+    /**
+     * Поиск ближайшего родителя у которого есть брендинг
+     *
+     * @return int
+     */
+    private function findNearestParentBrandID(){
+        //список родителей
+        $parents = array_keys(E()->getMap()->getParents($this->document->getID()));
+        //добавили текущий раздел
+        array_push($parents, $this->document->getID());
+
+        //делаем выборку брендингов
+        $d = convertDBResult($this->dbh->select('share_sitemap', array('smap_id', 'brand_id'), array('smap_id' => $parents)),
+                    'smap_id', true);
+        //Результирующий массив
+        $res = array();
+        //проходимся по списку родителей формируя отсортированный список "идент раздела"=>"идент бренда"
+        foreach($parents as $smapID){
+            if(!is_null($d[$smapID]['brand_id']))
+                $res[$smapID] = $d[$smapID]['brand_id'];
+        }
+        //возвращаем последний идент бренда
+        return array_pop($res);
+    }
+
+    protected function createBuilder() {
+        return new SimpleBuilder();
+    }
 }

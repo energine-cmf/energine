415fc79d65ff3080cba02be107f609c0d6c8732a
drPavka <d.pavka@gmail.com>
2012-01-09 15:28:29 +0000

    Переписан под измененный аттачментменеджер
    git-svn-id: https://energine.googlecode.com/svn/trunk@1946 acdd3125-bb50-0410-93b3-95ffde4b7c7d

--- core/modules/share/components/PageMedia.class.php
+++ core/modules/share/components/PageMedia.class.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * Содержит класс PageMedia
  *
@@ -8,37 +8,43 @@
  * @copyright d.pavka@gmail.com
  */
 
- /**
-  * Выводит на странице медиаконтейнер из меди файлов присоединенных к данной странице
-  *
-  * @package energine
-  * @subpackage share
-  * @author d.pavka@gmail.com
-  */
- class PageMedia extends DataSet {
+/**
+ * Выводит на странице медиаконтейнер из меди файлов присоединенных к данной странице
+ *
+ * @package energine
+ * @subpackage share
+ * @author d.pavka@gmail.com
+ */
+class PageMedia extends DataSet {
     /**
      * Конструктор класса
      *
      * @param string $name
      * @param string $module
-
      * @param array $params
      * @access public
      */
-    public function __construct($name, $module,   array $params = null) {
-        parent::__construct($name, $module,  $params);
+    public function __construct($name, $module, array $params = null) {
+        parent::__construct($name, $module, $params);
     }
-    
+
     /**
-      * Выводит галлерею
-      * 
-      * @return void
-      * @access protected
-      */
-    protected function main(){
+     * Выводит галлерею
+     *
+     * @return void
+     * @access protected
+     */
+    protected function main() {
         $this->prepare();
-        $am = new AttachmentManager($this->getDataDescription(), $this->getData(), 'share_sitemap');
-        $am->createFieldDescription();
-        $am->createField('smap_id', false, $this->document->getID());
+
+        //Поле добавлено чтобы Data не был пустым
+        $this->getData()->addField(new Field('fake'));
+        $m = new AttachmentManager(
+            $this->getDataDescription(),
+            $this->getData(),
+            'share_sitemap'
+        );
+        $m->createFieldDescription();
+        $m->createField('smap_id', false, $this->document->getID());
     }
 }

b77c12b807e2863bb4023891cda78a8dbb9c70a6
wildex <wildex1@gmail.com>
2012-01-13 14:27:09 +0000

    Добавлен фильтр по smap_id в ф-цию changeOrder
    git-svn-id: https://energine.googlecode.com/svn/trunk@1950 acdd3125-bb50-0410-93b3-95ffde4b7c7d

--- core/modules/apps/components/FeedEditor.class.php
+++ core/modules/apps/components/FeedEditor.class.php
@@ -83,4 +83,84 @@ class FeedEditor extends LinkingEditor {
 		return $result;
 	}
 
+    /**
+     * Изменяет порядок следования
+     *
+     * @param string  - направление
+     * @return void
+     * @access protected
+     */
+
+    protected function changeOrder($direction) {
+
+
+        if (!$this->getOrderColumn()) {
+            //Если не задана колонка для пользовательской сортировки то на выход
+            throw new SystemException('ERR_NO_ORDER_COLUMN', SystemException::ERR_DEVELOPER);
+        }
+
+        $currentID = $this->getStateParams();
+        list($currentID) = $currentID;
+
+        //Определяем order_num текущей страницы
+        $currentOrderNum = simplifyDBResult(
+            $this->dbh->selectRequest(
+                'SELECT ' . $this->getOrderColumn() . ' ' .
+                        'FROM ' . $this->getTableName() . ' ' .
+                        'WHERE ' . $this->getPK() . ' = %s',
+                $currentID
+            ),
+            $this->getOrderColumn(),
+            true
+        );
+
+        $orderDirection = ($direction == Grid::DIR_DOWN) ? QAL::ASC : QAL::DESC;
+
+        $baseFilter = $this->getFilter();
+
+        if (!empty($baseFilter)) {
+            $baseFilter = ' AND ' .
+                    str_replace('WHERE', '', $this->dbh->buildWhereCondition($this->getFilter()));
+        }
+        else {
+            $baseFilter = ' AND smap_id = '.$this->document->getID().' ';
+        }
+
+        //Определяем идентификатор записи которая находится рядом с текущей
+        $request =
+                'SELECT ' . $this->getPK() . ' as neighborID, ' .
+                        $this->getOrderColumn() . ' as neighborOrderNum ' .
+                        'FROM ' . $this->getTableName() . ' ' .
+                        'WHERE ' . $this->getOrderColumn() . ' ' . $direction .
+                        ' ' . $currentOrderNum . ' ' . $baseFilter .
+                        'ORDER BY ' . $this->getOrderColumn() . ' ' .
+                        $orderDirection . ' Limit 1';
+
+        $data =
+                convertDBResult($this->dbh->selectRequest($request), 'neighborID');
+        if ($data) {
+            extract(current($data));
+            $this->dbh->beginTransaction();
+            $this->dbh->modify(
+                QAL::UPDATE,
+                $this->getTableName(),
+                array($this->getOrderColumn() => $neighborOrderNum),
+                array($this->getPK() => $currentID)
+            );
+            $this->dbh->modify(
+                QAL::UPDATE,
+                $this->getTableName(),
+                array($this->getOrderColumn() => $currentOrderNum),
+                array($this->getPK() => $neighborID)
+            );
+            $this->dbh->commit();
+        }
+        $b = new JSONCustomBuilder();
+        $b->setProperties(array(
+            'result' => true,
+            'dir' => $direction
+        ));
+        $this->setBuilder($b);
+    }
+
 }

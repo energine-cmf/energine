7429db9a2f81f5af90235666e2894130e77de117
drPavka <d.pavka@gmail.com>
2012-01-17 15:57:01 +0000

    git-svn-id: https://energine.googlecode.com/svn/trunk@1958 acdd3125-bb50-0410-93b3-95ffde4b7c7d

--- core/modules/user/scripts/Register.js
+++ core/modules/user/scripts/Register.js
@@ -1,42 +1,45 @@
 ScriptLoader.load('ValidForm');
 
-var Register  = new Class({
-    Extends: ValidForm,
-    
-    initialize: function(element){
+var Register = new Class({
+    Extends:ValidForm,
+
+    initialize:function (element) {
         this.parent(element);
-        
-        if(this.componentElement){
+
+        if (this.componentElement) {
             this.loginField = this.form.getElementById('u_name');
             this.registerButton = this.form.getElement('button[name=register]');
-            this.loginField.addEvent('blur', function(event){
+            this.loginField.addEvent('blur', function (event) {
+
                 var event = new Event(event || window.event);
-                if(this.validator.validateElement(event.target)){
-                    this.checkLogin(this.loginField.get('value'));            
-                }
-                else {
-                    this.registerButton.setProperty('disabled', 'disabled');
-                    this.cancelEvent(event);
+                if (event.target.value) {
+                    if (this.validator.validateElement(event.target)) {
+                        this.checkLogin(this.loginField.get('value'));
+                    }
+                    else {
+                        if(this.registerButton)this.registerButton.setProperty('disabled', 'disabled');
+                        this.cancelEvent(event);
+                    }
                 }
             }.bind(this));
             this.captchaField = this.form.getElementById('captcha');
             this.captchaImage = this.form.getElementById('captchaImage');
         }
-	},
-    checkLogin: function(loginValue){
+    },
+    checkLogin:function (loginValue) {
         new Request.JSON({
-            url:this.componentElement.getProperty('single_template')+'check/',
-            method: 'post',
-            onSuccess: function(response){
-                if(!response.result){
-                    this.validator.showError(this.loginField, response.message);       
-                    this.registerButton.setProperty('disabled', 'disabled');
+            url:this.componentElement.getProperty('single_template') + 'check/',
+            method:'post',
+            onSuccess:function (response) {
+                if (!response.result) {
+                    this.validator.showError(this.loginField, response.message);
+                    if(this.registerButton)this.registerButton.setProperty('disabled', 'disabled');
                 }
-                else{
-                    this.registerButton.removeProperty('disabled');
+                else {
+                    if(this.registerButton)this.registerButton.removeProperty('disabled');
                 }
             }.bind(this)
         }).send('login=' + loginValue);
-    
+
     }
 });

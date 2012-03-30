95ddd8658de331af42ed8fce0861877e0fcca1de
drPavka <d.pavka@gmail.com>
2012-03-30 10:39:42 +0000

    Исправлена очистка кеша при внесении измений в таблицу переводов
    git-svn-id: https://energine.googlecode.com/svn/trunk@2046 acdd3125-bb50-0410-93b3-95ffde4b7c7d

--- core/modules/share/components/TranslationEditor.class.php
+++ core/modules/share/components/TranslationEditor.class.php
@@ -22,36 +22,37 @@ class TranslationEditor extends Grid {
      *
      * @param string $name
      * @param string $module
-
      * @param array $params
      * @access public
      */
-	public function __construct($name, $module,   array $params = null) {
-        parent::__construct($name, $module,  $params);
+    public function __construct($name, $module, array $params = null) {
+        parent::__construct($name, $module, $params);
         $this->setTableName('share_lang_tags');
-        $this->setOrder(array('ltag_name' => QAL::ASC ));
-	}
-	
-	protected function prepare(){
-		parent::prepare();
-		if(in_array($this->getState(), array('add', 'edit'))){
-			$this->getDataDescription()->getFieldDescriptionByName('ltag_value_rtf')->setType(FieldDescription::FIELD_TYPE_TEXT);
-		}
-	}
+        $this->setOrder(array('ltag_name' => QAL::ASC));
+    }
 
-	protected function saveData(){
-        $c = E()->getCache();
-        if($c->isEnabled()){
-            $c->dispose(Cache::TRANSLATIONS_KEY);
+    protected function prepare() {
+        parent::prepare();
+        if (in_array($this->getState(), array('add', 'edit'))) {
+            $this->getDataDescription()->getFieldDescriptionByName('ltag_value_rtf')->setType(FieldDescription::FIELD_TYPE_TEXT);
         }
+    }
+
+    protected function saveData() {
         //обрезаем лишние незначащие пробелы и прочее в самих тегах и в переводах
         //в переводах - сделано на случай вывода в джаваскрипт
-		$_POST[$this->getTableName()]['ltag_name'] = strtoupper(trim($_POST[$this->getTableName()]['ltag_name']));
-		foreach(array_keys(E()->getLanguage()->getLanguages()) as $langID){
-            if(isset($_POST[$this->getTranslationTableName()][$langID]['ltag_value_rtf'])){
+        $_POST[$this->getTableName()]['ltag_name'] = strtoupper(trim($_POST[$this->getTableName()]['ltag_name']));
+        foreach (array_keys(E()->getLanguage()->getLanguages()) as $langID) {
+            if (isset($_POST[$this->getTranslationTableName()][$langID]['ltag_value_rtf'])) {
                 $_POST[$this->getTranslationTableName()][$langID]['ltag_value_rtf'] = trim($_POST[$this->getTranslationTableName()][$langID]['ltag_value_rtf']);
             }
         }
-		return parent::saveData();
-	}
+        $result = parent::saveData();
+
+        $c = E()->getCache();
+        if ($c->isEnabled()) {
+            $c->dispose(Cache::TRANSLATIONS_KEY);
+        }
+        return $result;
+    }
 }

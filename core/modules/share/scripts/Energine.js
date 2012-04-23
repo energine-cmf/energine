68536bdf3607feb3e8ac96c5fbfe1ac5d1c47dc9
YNnoer@gmail.com <YNnoer@gmail.com>
2012-04-23 14:41:50 +0000

    git-svn-id: https://energine.googlecode.com/svn/trunk@2076 acdd3125-bb50-0410-93b3-95ffde4b7c7d

--- core/modules/share/scripts/Energine.js
+++ core/modules/share/scripts/Energine.js
@@ -3,7 +3,8 @@
  */
 
 var ScriptLoader = function() {
-    window.top.currentWindow = window;
+    if(typeof(embed_external)!='undefined'){
+            window.top.currentWindow = window;
 
     return window.top.ScriptLoader || {
         request : null,
@@ -71,6 +72,78 @@ var ScriptLoader = function() {
             w.eval(code);
         }
     };
+    } else {
+
+        window.currentWindow = window;
+
+        return window.ScriptLoader || {
+                request : null,
+                loaded : {},
+                load : function() {
+                    var filename;
+                    for (var i = 0, len = arguments.length; i < len; i++) {
+                        filename = arguments[i];
+                        //Проверяем загружен ли файл
+                        if (!this.loaded[filename]) {
+                            //Если файл не загружен
+                            if (!this.request) {
+                                //И нет XMLHTTRequest - создаем
+                                this.request = window.XMLHttpRequest
+                                        ? new XMLHttpRequest
+                                        : (Browser.Engine.trident
+                                        ? new ActiveXObject('Microsoft.XMLHTTP')
+                                        : null);
+                            }
+                            if (!this.request)
+                                throw 'Ajax request is not created';
+
+                            this.request
+                                    .open('GET', ((Energine.base)?Energine.base:'')
+                                    + 'scripts/'
+                                    + filename
+                                    + '.js'
+                                    + ((Energine.debug) ? '?'
+                                    + Math.random() : ''), false);
+                            this.request.send(null);
+                            //получаем текст запрашиваемого файла
+                            if (this.request.status == 200) {
+                                this.loaded[filename] = [];
+                                this.loaded[filename]['code'] =
+                                        this.request.responseText;
+                            }
+                        }
+
+                        if (!this.loaded[filename] || !this.loaded[filename]['code'])
+                            throw 'Invalid file code. Filename: ' + filename;
+
+                        //На этот момент у нас есть текст запрашиваемого файла
+                        //Но он может быть исполнен в одном из открытых окон а в другом - нет
+                        this.globalEval(this.loaded[filename]['code']);
+                    }
+                },
+                isLoadedInCurrentWindow: function(arr) {
+                    var result = false;
+                    for (var i = 0, l = arr.length; i < l; i++) {
+                        if (arr[i] === window.currentWindow) {
+                            result = true;
+                            break;
+                        }
+                    }
+                    return result;
+                },
+                globalEval : function(code) {
+                    var w = window.currentWindow;
+                    if (w.execScript) {
+                        w.execScript(code, 'javascript');
+                        return;
+                    }
+
+                    //            (function(){w.eval.call(w, code)})();
+                    w.eval(code);
+                }
+            };
+       }
+
 }();
 
 

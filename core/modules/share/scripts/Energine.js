1bac5764844c2079abc416caf55b53aa83ffd5a1
drPavka <d.pavka@gmail.com>
2012-06-06 16:23:52 +0000

    обновлена версия мутулз
    git-svn-id: https://energine.googlecode.com/svn/trunk@2094 acdd3125-bb50-0410-93b3-95ffde4b7c7d

--- core/modules/share/scripts/Energine.js
+++ core/modules/share/scripts/Energine.js
@@ -2,14 +2,12 @@
  * Загружает указанные скрипты из директории scripts.
  */
 
-var ScriptLoader = function() {
-    if(typeof(embed_external)!='undefined'){
-            window.top.currentWindow = window;
-
+var ScriptLoader = function () {
+    window.top.currentWindow = window;
     return window.top.ScriptLoader || {
-        request : null,
-        loaded : {},
-        load : function() {
+        request:null,
+        loaded:{},
+        load:function () {
             var filename;
             for (var i = 0, len = arguments.length; i < len; i++) {
                 filename = arguments[i];
@@ -19,27 +17,27 @@ var ScriptLoader = function() {
                     if (!this.request) {
                         //И нет XMLHTTRequest - создаем
                         this.request = window.XMLHttpRequest
-                                ? new XMLHttpRequest
-                                : (Browser.Engine.trident
-                                ? new ActiveXObject('Microsoft.XMLHTTP')
-                                : null);
+                            ? new XMLHttpRequest
+                            : (Browser.Engine.trident
+                            ? new ActiveXObject('Microsoft.XMLHTTP')
+                            : null);
                     }
                     if (!this.request)
                         throw 'Ajax request is not created';
 
                     this.request
-                            .open('GET', ((Energine.base)?Energine.base:'')
-                            + 'scripts/'
-                            + filename
-                            + '.js'
-                            + ((Energine.debug) ? '?'
-                            + Math.random() : ''), false);
+                        .open('GET', ((Energine.base) ? Energine.base : '')
+                        + 'scripts/'
+                        + filename
+                        + '.js'
+                        + ((Energine.debug) ? '?'
+                        + Math.random() : ''), false);
                     this.request.send(null);
                     //получаем текст запрашиваемого файла
                     if (this.request.status == 200) {
                         this.loaded[filename] = [];
                         this.loaded[filename]['code'] =
-                                this.request.responseText;
+                            this.request.responseText;
                     }
                 }
 
@@ -51,7 +49,7 @@ var ScriptLoader = function() {
                 this.globalEval(this.loaded[filename]['code']);
             }
         },
-        isLoadedInCurrentWindow: function(arr) {
+        isLoadedInCurrentWindow:function (arr) {
             var result = false;
             for (var i = 0, l = arr.length; i < l; i++) {
                 if (arr[i] === window.top.currentWindow) {
@@ -61,7 +59,7 @@ var ScriptLoader = function() {
             }
             return result;
         },
-        globalEval : function(code) {
+        globalEval:function (code) {
             var w = window.top.currentWindow;
             if (w.execScript) {
                 w.execScript(code, 'javascript');
@@ -72,106 +70,34 @@ var ScriptLoader = function() {
             w.eval(code);
         }
     };
-    } else {
-
-        window.currentWindow = window;
-
-        return window.ScriptLoader || {
-                request : null,
-                loaded : {},
-                load : function() {
-                    var filename;
-                    for (var i = 0, len = arguments.length; i < len; i++) {
-                        filename = arguments[i];
-                        //Проверяем загружен ли файл
-                        if (!this.loaded[filename]) {
-                            //Если файл не загружен
-                            if (!this.request) {
-                                //И нет XMLHTTRequest - создаем
-                                this.request = window.XMLHttpRequest
-                                        ? new XMLHttpRequest
-                                        : (Browser.Engine.trident
-                                        ? new ActiveXObject('Microsoft.XMLHTTP')
-                                        : null);
-                            }
-                            if (!this.request)
-                                throw 'Ajax request is not created';
-
-                            this.request
-                                    .open('GET', ((Energine.base)?Energine.base:'')
-                                    + 'scripts/'
-                                    + filename
-                                    + '.js'
-                                    + ((Energine.debug) ? '?'
-                                    + Math.random() : ''), false);
-                            this.request.send(null);
-                            //получаем текст запрашиваемого файла
-                            if (this.request.status == 200) {
-                                this.loaded[filename] = [];
-                                this.loaded[filename]['code'] =
-                                        this.request.responseText;
-                            }
-                        }
-
-                        if (!this.loaded[filename] || !this.loaded[filename]['code'])
-                            throw 'Invalid file code. Filename: ' + filename;
-
-                        //На этот момент у нас есть текст запрашиваемого файла
-                        //Но он может быть исполнен в одном из открытых окон а в другом - нет
-                        this.globalEval(this.loaded[filename]['code']);
-                    }
-                },
-                isLoadedInCurrentWindow: function(arr) {
-                    var result = false;
-                    for (var i = 0, l = arr.length; i < l; i++) {
-                        if (arr[i] === window.currentWindow) {
-                            result = true;
-                            break;
-                        }
-                    }
-                    return result;
-                },
-                globalEval : function(code) {
-                    var w = window.currentWindow;
-                    if (w.execScript) {
-                        w.execScript(code, 'javascript');
-                        return;
-                    }
-
-                    //            (function(){w.eval.call(w, code)})();
-                    w.eval(code);
-                }
-            };
-       }
-
 }();
 
 
-var isset = function(variable) {
+var isset = function (variable) {
     return ('undefined' != typeof(variable));
 }
 /*
-var $chk = function(obj){
-    return !!(obj || obj === 0);
-};
-*/
+ var $chk = function(obj){
+ return !!(obj || obj === 0);
+ };
+ */
 
 var Energine = {
-    debug : false,
-    base : null,
-    translations: new Hash(),
-    forceJSON: false,
-    supportContentEdit : Browser.ie || Browser.firefox ||
-            Browser.opera || Browser.chrome
+    debug:false,
+    base:null,
+    translations:new Hash(),
+    forceJSON:false,
+    supportContentEdit:Browser.ie || Browser.firefox ||
+        Browser.opera || Browser.chrome
 }
 Energine.request = {
-    request : function(uri, data, onSuccess, onUserError, onServerError, method) {
-        onServerError = onServerError || function(responseText){
+    request:function (uri, data, onSuccess, onUserError, onServerError, method) {
+        onServerError = onServerError || function (responseText) {
 
         };
         method = method || 'post';
-        var callbackFunction = function(response, responseText) {
-            if(!response){
+        var callbackFunction = function (response, responseText) {
+            if (!response) {
                 onServerError(responseText);
                 return;
             }
@@ -180,9 +106,9 @@ Energine.request = {
                 onSuccess(response);
             } else {
                 var msg = (typeof response.title != 'undefined')
-                        ? response.title
-                        : 'Произошла ошибка:' + "\n";
-                response.errors.each(function(error) {
+                    ? response.title
+                    : 'Произошла ошибка:' + "\n";
+                response.errors.each(function (error) {
                     if (typeof error.field != 'undefined') {
                         msg += error.field + " :\t";
                     }
@@ -199,13 +125,14 @@ Energine.request = {
             }
         };
         new Request.JSON({
-            'url' : uri + ((Energine.forceJSON)?'?json':''),
-            'method' : method,
-            'data' : data,
+            'url':uri + ((Energine.forceJSON) ? '?json' : ''),
+            'method':method,
+            'data':data,
             // 'noCache': true,
-            'evalResponse' : false,
-            'onComplete' : callbackFunction,
-            'onFailure': function(e){/*console.log(arguments)*/}
+            'evalResponse':false,
+            'onComplete':callbackFunction,
+            'onFailure':function (e) {/*console.log(arguments)*/
+            }
         }).send();
 
     }
@@ -224,35 +151,35 @@ Energine.cancelEvent = function (e) {
             e.cancelBubble = true;
         }
     }
-    catch(exc) {
+    catch (exc) {
     }
     return false;
 }
 
-Energine.createDatePicker = function(datePickerObj, nullable) {
+Energine.createDatePicker = function (datePickerObj, nullable) {
     var props = {
         format:'j-m-Y',
-        allowEmpty: nullable,
-        inputOutputFormat: 'Y-m-d',
-        useFadeInOut: false
+        allowEmpty:nullable,
+        inputOutputFormat:'Y-m-d',
+        useFadeInOut:false
     };
     return Energine._createDatePickerObject($(datePickerObj), props);
 }
 
-Energine.createDateTimePicker = function(datePickerObj, nullable) {
+Energine.createDateTimePicker = function (datePickerObj, nullable) {
     //DateTime
     var props = {
-        timePicker: true,
-        format: 'j-m-Y H:i',
-        inputOutputFormat: 'Y-m-d H:i',
-        allowEmpty: nullable,
-        useFadeInOut: false
+        timePicker:true,
+        format:'j-m-Y H:i',
+        inputOutputFormat:'Y-m-d H:i',
+        allowEmpty:nullable,
+        useFadeInOut:false
     }
 
     return Energine._createDatePickerObject($(datePickerObj), props);
 }
 
-Energine._createDatePickerObject = function(datePickerObj, props) {
+Energine._createDatePickerObject = function (datePickerObj, props) {
 
     if (!isset(this.datePickerDataLoaded)) {
         Asset.css('datepicker.css');
@@ -260,10 +187,10 @@ Energine._createDatePickerObject = function(datePickerObj, props) {
         this.datePickerDataLoaded = true;
     }
     var dp = new DatePicker(datePickerObj, $extend({
-        //debug:true    
-    },
-            props
-            ));
+            //debug:true
+        },
+        props
+    ));
 
     //dp.input.set('value', dp.visual.get('value'));
     return dp;
@@ -273,31 +200,31 @@ Energine._createDatePickerObject = function(datePickerObj, props) {
  * stylesheets.
  */
 Asset.loaded = {
-    css : {}
+    css:{}
 };
-Asset.css = function(source, properties) {
+Asset.css = function (source, properties) {
     if (!Asset.loaded.css[source]) {
         Asset.loaded.css[source] = true;
         properties = properties || {};
 
         var result = new Element('link');
         result.setProperties($merge({
-            'rel' : 'stylesheet',
-            'media' : 'Screen, projection',
-            'type' : 'text/css',
-            'href' : ((Energine.static) ? Energine.static : '') + 'stylesheets/' +
-                    source
-            }, properties));
+            'rel':'stylesheet',
+            'media':'Screen, projection',
+            'type':'text/css',
+            'href':((Energine.static) ? Energine.static : '') + 'stylesheets/' +
+                source
+        }, properties));
         result.inject(document.head);
         return result;
     }
     return false;
 }
 
-var showhideField = function(obj, event) {
+var showhideField = function (obj, event) {
     Energine.cancelEvent(event);
     var field = $(obj).getParents('.field')[0];
-    if (field.hasClass('min')){
+    if (field.hasClass('min')) {
         field.removeClass('min').addClass('max');
         var richEditor = field.getElement('.richEditor');
         if (richEditor) richEditor.focus();

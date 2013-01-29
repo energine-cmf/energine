06f152400cc06a56986862e1c113b0116f4f1fa7
drPavka <d.pavka@gmail.com>
2013-01-29 15:13:19 +0000

    git-svn-id: https://energine.googlecode.com/svn/trunk@2201 acdd3125-bb50-0410-93b3-95ffde4b7c7d

--- core/kernel/Document.class.php
+++ core/kernel/Document.class.php
@@ -235,7 +235,7 @@ final class Document extends DBWorker implements IDocument {
         $prop->setAttribute('media', (($mediaURL =
             $this->getConfigValue('site.media')) ? $mediaURL : $baseURL));
         $prop->setAttribute('resizer', (($resizerURL =
-            $this->getConfigValue('site.resizer')) ? $resizerURL : $baseURL));
+            $this->getConfigValue('site.resizer')) ? $resizerURL : ($baseURL.'resizer/')));
         $prop->setAttribute('folder', E()->getSiteManager()->getCurrentSite()->folder);
         $prop->setAttribute('default', E()->getSiteManager()->getDefaultSite()->base);
         $dom_documentProperties->appendChild($prop);

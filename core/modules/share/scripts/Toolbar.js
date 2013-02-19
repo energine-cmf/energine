16387262a04a49df215c32519f0594d5816e970b
wildex <wildex1@gmail.com>
2013-02-19 13:55:30 +0000

    Фикс поведения свитчера для Хрома
    git-svn-id: https://energine.googlecode.com/svn/trunk@2211 acdd3125-bb50-0410-93b3-95ffde4b7c7d

--- core/modules/share/scripts/Toolbar.js
+++ core/modules/share/scripts/Toolbar.js
@@ -320,7 +320,7 @@ Toolbar.Switcher = new Class({
                     this.element.removeClass('pressed');
             }
         }).bind(this);
-        this.element.addEvent('mousedown', function () {
+        this.element.addEvent('click', function () {
             if (!this.properties.disabled) {
                 this.properties.state = (!this.properties.state);
                 toggle();

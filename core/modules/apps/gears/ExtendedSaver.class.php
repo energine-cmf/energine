fbda6594b758ca23df3ca93aa5759c42c9f40dd3
drPavka <d.pavka@gmail.com>
2013-02-25 11:22:21 +0000

    http://redmine.eggmen.net/issues/4833
    git-svn-id: https://energine.googlecode.com/svn/trunk@2212 acdd3125-bb50-0410-93b3-95ffde4b7c7d

--- core/modules/apps/gears/ExtendedSaver.class.php
+++ core/modules/apps/gears/ExtendedSaver.class.php
@@ -8,9 +8,13 @@ class ExtendedSaver extends Saver {
      * @var string
      */
     private $pk;
-    
 
+    /**
+     * Имя основной таблицы
+     * @var string
+     */
     private $mainTableName;
+
     /**
      * Устанавливает имя основной таблицы
      *
@@ -29,16 +33,30 @@ class ExtendedSaver extends Saver {
     }
 
     /**
+     * @return string
+     */
+    protected function getTableName() {
+        return $this->mainTableName;
+    }
+
+    /**
+     * @return string
+     */
+    protected function getPK(){
+        return $this->pk;
+    }
+
+    /**
      * Сохраняем данные в таблицу аплоадсов и в таблицу тегов
      * @return void
      */
     public function save() {
         $result = parent::save();
         $entityID = ($this->getMode() == QAL::INSERT) ? $result
-                : $this->getData()->getFieldByName($this->pk)->getRowData(0);
+            : $this->getData()->getFieldByName($this->pk)->getRowData(0);
         $tm = new TagManager($this->dataDescription, $this->data, $this->mainTableName);
         $tm->save($entityID);
-        
+
         $am = new AttachmentManager($this->dataDescription, $this->data, $this->mainTableName);
         $am->save($entityID);
 
--- core/modules/apps/gears/NewsEditorSaver.class.php
+++ core/modules/apps/gears/NewsEditorSaver.class.php
@@ -7,6 +7,28 @@ class NewsEditorSaver extends ExtendedSaver {
         if (!$data->getFieldByName('news_segment')->getRowData(0)) {
             $f = new Field('news_segment');
             $translitedTitle = Translit::asURLSegment($data->getFieldByName('news_title')->getRowData(0));
+            $filter = array();
+            $pkF = $this->getData()->getFieldByName($this->getPK());
+
+            if($pkF = $pkF->getRowData(0)){
+                $filter[$this->getPK()] = $pkF;
+            }
+            for($i=0; $i<10; $i++){
+                $tempTitle = $translitedTitle;
+                if($i){
+                    $tempTitle = $translitedTitle.'-'.$i;
+                }
+
+                $filter = array('news_segment' => $tempTitle);
+                if(!$this->dbh->getScalar($this->getTableName(), 'news_segment', $filter)){
+                    break;
+                }
+                else {
+                    $tempTitle = $translitedTitle.'-'.$i;
+                }
+            }
+
+            $translitedTitle = $tempTitle;
             for ($i = 0, $l = sizeof(E()->getLanguage()->getLanguages()); $i < $l; $i++)
                 $f->setRowData($i, $translitedTitle);
 

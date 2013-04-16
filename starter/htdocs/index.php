a880f81f7a7ba7f87323da3fbeac70cc8677df60
andykarpov <andy.karpov@gmail.com>
2013-04-16 15:14:53 +0000

    Добавлена опция вывода замеры времени рендеринга страницы в  custom header X-Timer (отключаемая через конфиг)
    git-svn-id: https://energine.googlecode.com/svn/branches/2.11.2.dev@2278 acdd3125-bb50-0410-93b3-95ffde4b7c7d

--- starter/htdocs/index.php
+++ starter/htdocs/index.php
@@ -12,9 +12,22 @@ try {
     // подключаем bootstrap
     require_once('bootstrap.php');
 
+    $use_timer = E()->getConfigValue('site.useTimer');
+    if ($use_timer) {
+        class Timer Extends Object { }
+        $timer = new Timer();
+        $timer->startTimer();
+    }
+
     UserSession::start();
     $reg = E();
     $reg->getController()->run();
+
+    if ($use_timer) {
+        $timer->stopTimer();
+        $reg->getResponse()->setHeader('X-Timer', $timer->getTimer());
+    }
+
     $reg->getResponse()->commit();
 }
 catch (Exception $generalException) {

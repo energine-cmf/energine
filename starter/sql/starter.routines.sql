a7f9179282377055482cd90e1f09b8cb3c1ee63d
drPavka <d.pavka@gmail.com>
2013-05-02 20:46:25 +0000

    Добавлены процедура и функция для построения хлебных крошек  в файловом репозитории
    git-svn-id: https://energine.googlecode.com/svn/branches/2.11.2.dev@2320 acdd3125-bb50-0410-93b3-95ffde4b7c7d

--- starter/sql/starter.routines.sql
+++ starter/sql/starter.routines.sql
@@ -57,6 +57,25 @@ BEGIN
 		SELECT media_id INTO repo_id;
 	END IF;
 END ;;
+
+CREATE FUNCTION `get_upl_parent`(`in_id` INT(10) UNSIGNED) RETURNS int(10) unsigned
+    READS SQL DATA
+RETURN (select ifnull(`upl_pid`, 0) from `share_uploads` where `upl_id` = in_id);;
+
+CREATE PROCEDURE `proc_get_upl_pid_list`(IN `in_id` INT(10) UNSIGNED)
+    MODIFIES SQL DATA
+BEGIN
+	create temporary table if not exists temp (id int(10) UNSIGNED, title varchar(255));
+	set @_pid = in_id;
+
+	while @_pid > 0 do
+		insert into `temp` select upl_id, upl_title from `share_uploads` where upl_id = @_pid;
+		set @_pid = get_upl_parent(@_pid);
+	end while;
+	select * from `temp`;
+	drop table if exists `temp`;
+END;;
+
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;

9e797bbfb684b2c557a6df14e52fb44f1fa2c8b4
andykarpov <andy.karpov@gmail.com>
2013-05-07 16:07:36 +0000

    Исправлен код создания хранимых процедур
    git-svn-id: https://energine.googlecode.com/svn/trunk@2325 acdd3125-bb50-0410-93b3-95ffde4b7c7d

--- starter/sql/starter.routines.sql
+++ starter/sql/starter.routines.sql
@@ -1,94 +1,70 @@
--- MySQL dump
---
--- Host: dbhost    Database: starter
--- ------------------------------------------------------
+SET NAMES utf8;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET UNIQUE_CHECKS         = 0;
+SET FOREIGN_KEY_CHECKS    = 0;
+SET character_set_client  = utf8;
+SET character_set_results = utf8;
+SET collation_connection  = utf8_general_ci;
+SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION';
 
-/*!50003 DROP FUNCTION IF EXISTS `get_share_uploads_repo_id` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8 */ ;
-/*!50003 SET character_set_results = utf8 */ ;
-/*!50003 SET collation_connection  = utf8_general_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DROP FUNCTION IF EXISTS `get_share_uploads_repo_id`;
+
 DELIMITER ;;
+
 CREATE FUNCTION `get_share_uploads_repo_id`(media_id INT UNSIGNED) RETURNS int(11)
-    DETERMINISTIC
-BEGIN
-	DECLARE upl_pid INT;
-	SET upl_pid = 0;
-	CALL proc_get_share_uploads_repo_id(media_id, upl_pid);
-	RETURN upl_pid;
-END ;;
+DETERMINISTIC
+  BEGIN
+    DECLARE upl_pid INT;
+    SET upl_pid = 0;
+    CALL proc_get_share_uploads_repo_id(media_id, upl_pid);
+    RETURN upl_pid;
+  END ;;
+
 DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `proc_get_share_uploads_repo_id` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8 */ ;
-/*!50003 SET character_set_results = utf8 */ ;
-/*!50003 SET collation_connection  = utf8_general_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+
+DROP PROCEDURE IF EXISTS `proc_get_share_uploads_repo_id`;
+
 DELIMITER ;;
+
 CREATE PROCEDURE `proc_get_share_uploads_repo_id`(IN media_id INT UNSIGNED, OUT repo_id INT UNSIGNED)
-BEGIN
-	SET max_sp_recursion_depth=100;
-	SELECT upl_pid INTO repo_id FROM share_uploads WHERE upl_id = media_id;
-	IF repo_id > 0 THEN
-		CALL proc_get_share_uploads_repo_id(repo_id, repo_id);
-	ELSE
-		SELECT media_id INTO repo_id;
-	END IF;
-END ;;
+  BEGIN
+    SET max_sp_recursion_depth=100;
+    SELECT upl_pid INTO repo_id FROM share_uploads WHERE upl_id = media_id;
+    IF repo_id > 0 THEN
+      CALL proc_get_share_uploads_repo_id(repo_id, repo_id);
+    ELSE
+      SELECT media_id INTO repo_id;
+    END IF;
+  END ;;
+
+DELIMITER ;
+
+DROP FUNCTION IF EXISTS `get_upl_parent`;
+
+DELIMITER ;;
 
 CREATE FUNCTION `get_upl_parent`(`in_id` INT(10) UNSIGNED) RETURNS int(10) unsigned
-    READS SQL DATA
-RETURN (select ifnull(`upl_pid`, 0) from `share_uploads` where `upl_id` = in_id);;
-
-CREATE PROCEDURE `proc_get_upl_pid_list`(IN `in_id` INT(10) UNSIGNED)
-    MODIFIES SQL DATA
-BEGIN
-	create temporary table if not exists temp (id int(10) UNSIGNED, title varchar(255));
-	set @_pid = in_id;
-
-	while @_pid > 0 do
-		insert into `temp` select upl_id, upl_title from `share_uploads` where upl_id = @_pid;
-		set @_pid = get_upl_parent(@_pid);
-	end while;
-	select * from `temp`;
-	drop table if exists `temp`;
-END;;
+READS SQL DATA
+  RETURN (select ifnull(`upl_pid`, 0) from `share_uploads` where `upl_id` = in_id);;
 
 DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+DROP PROCEDURE IF EXISTS `proc_get_upl_pid_list`;
 
--- Dump completed on 2013-04-05 21:52:13
+DELIMITER ;;
+
+CREATE PROCEDURE `proc_get_upl_pid_list` (IN `in_id` INT(10) UNSIGNED)
+MODIFIES SQL DATA
+  BEGIN
+    create temporary table if not exists temp (id int(10) UNSIGNED, title varchar(255)) DEFAULT CHARSET=utf8;
+    set @_pid = in_id;
+
+    while @_pid > 0 do
+    insert into `temp` select upl_id, upl_title from `share_uploads` where upl_id = @_pid;
+    set @_pid = get_upl_parent(@_pid);
+    end while;
+    select * from `temp`;
+    drop table if exists `temp`;
+  END;;
+
+DELIMITER ;

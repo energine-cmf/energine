8f2b7cc4eb61cb703cd2f0fbbde749e45b73a637
drPavka <d.pavka@gmail.com>
2013-05-13 20:44:33 +0000

    По мелочи
    git-svn-id: https://energine.googlecode.com/svn/trunk@2349 acdd3125-bb50-0410-93b3-95ffde4b7c7d

--- core/modules/share/gears/FileRepositoryLocal.class.php
+++ core/modules/share/gears/FileRepositoryLocal.class.php
@@ -165,7 +165,7 @@ class FileRepositoryLocal extends Object implements IFileRepository {
         if (!file_exists($dir)) {
             mkdir($dir, 0777, true);
         }
-        if(!is_writable(dirname($dir))){
+        if(!is_writable($dir)){
             throw new SystemException('ERR_DIR_WRITE', SystemException::ERR_CRITICAL, $dir);
         }
         if (!copy($sourceFilename, $destFilename)) {

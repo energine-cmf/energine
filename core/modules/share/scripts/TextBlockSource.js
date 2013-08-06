94b1fd185220efdff12cbf30a4115ef9c5219d2a
drPavka <d.pavka@gmail.com>
2013-08-06 14:23:55 +0000

    Поправлен вывод поля кода
    git-svn-id: https://energine.googlecode.com/svn/trunk@2522 acdd3125-bb50-0410-93b3-95ffde4b7c7d

--- core/modules/share/scripts/TextBlockSource.js
+++ core/modules/share/scripts/TextBlockSource.js
@@ -1,9 +1,10 @@
 ScriptLoader.load('Form');
 var TextBlockSource = new Class({
 	Extends: Form,
-
     initialize: function(element) {
         this.parent(element);
+        this.componentElement.getElement('div.field').swapClass('min', 'max');
+
         this.codeEditors[0].setValue(window.top.ModalBox.getExtraData());
     },
 

c55ba6f0a6eb1244f6e78cacd12ec6a4e68c853e
drPavka <d.pavka@gmail.com>
2013-09-17 09:37:25 +0000

    убраны ненужные компоненты и конфиги к ним
    git-svn-id: https://energine.googlecode.com/svn/trunk@2578 acdd3125-bb50-0410-93b3-95ffde4b7c7d

--- core/modules/user/scripts/UserManager.js
+++ core/modules/user/scripts/UserManager.js
@@ -5,11 +5,5 @@ var UserManager  = new Class({
     activate: function(){
         
         this.request(this.singlePath + this.grid.getSelectedRecordKey() + '/activate/', null, this.loadPage.pass(this.pageList.currentPage, this));
-    },
-    ban: function() {
-        ModalBox.open({
-            url: this.singlePath + this.grid.getSelectedRecordKey() + '/ban/',
-            onClose:this.reload.bind(this)
-        });
     }
 });

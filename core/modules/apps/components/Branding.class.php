346d78aa9c9edd6a6f992f0088cd510d6aebdffc
drPavka <d.pavka@gmail.com>
2013-10-17 09:19:20 +0000

    поправлен Branding
    git-svn-id: https://energine.googlecode.com/svn/trunk@2671 acdd3125-bb50-0410-93b3-95ffde4b7c7d

--- core/modules/apps/components/Branding.class.php
+++ core/modules/apps/components/Branding.class.php
@@ -24,12 +24,13 @@ class Branding extends DBDataSet {
         $this->setFilter(array('brand_id' => $this->findNearestParentBrandID()));
         $this->setParam('recordsPerPage', false);
     }
+
     /**
      * Поиск ближайшего родителя у которого есть брендинг
      *
      * @return int
      */
-    private function findNearestParentBrandID(){
+    private function findNearestParentBrandID() {
         //список родителей
         $parents = array_keys(E()->getMap()->getParents($this->document->getID()));
         //добавили текущий раздел
@@ -37,12 +38,12 @@ class Branding extends DBDataSet {
 
         //делаем выборку брендингов
         $d = convertDBResult($this->dbh->select('share_sitemap', array('smap_id', 'brand_id'), array('smap_id' => $parents)),
-                    'smap_id', true);
+            'smap_id', true);
         //Результирующий массив
         $res = array();
         //проходимся по списку родителей формируя отсортированный список "идент раздела"=>"идент бренда"
-        foreach($parents as $smapID){
-            if(!is_null($d[$smapID]['brand_id']))
+        foreach ($parents as $smapID) {
+            if (!is_null($d[$smapID]['brand_id']))
                 $res[$smapID] = $d[$smapID]['brand_id'];
         }
         //возвращаем последний идент бренда
@@ -51,5 +52,14 @@ class Branding extends DBDataSet {
 
     protected function createBuilder() {
         return new SimpleBuilder();
+    }
+
+    protected function createDataDescription() {
+        $result = parent::createDataDescription();
+        $result->getFieldDescriptionByName('brand_main_img')->setType(FieldDescription::FIELD_TYPE_STRING);
+        foreach($result as $fd){
+            $fd->setMode(FieldDescription::FIELD_MODE_READ);
+        }
+        return $result;
     }
 }

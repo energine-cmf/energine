24c343dfe8e865810d9a58569a0c0b745ee5d927
valerii-zinchenko <formatcok@gmail.com>
2013-11-06 13:46:10 +0000

    Created abstract class for menus AMenu (Menu.js).
    git-svn-id: https://energine.googlecode.com/svn/trunk@2706 acdd3125-bb50-0410-93b3-95ffde4b7c7d

---
+++ core/modules/share/scripts/Menu.js
@@ -0,0 +1,146 @@
+/**
+ * @file Contain the description of the next classes:
+ * <ul>
+ *     <li>[GroupForm]{@link GroupForm}</li>
+ * </ul>
+ *
+ * @requires share/Form
+ *
+ * @author
+ *
+ * @version 1.0.0
+ */
+
+/**
+ * Menu. From MooTools it implements: Options, Events.
+ *
+ * @constructor
+ * @param {Element|string} el Main element or element ID.
+ * @param {Object} options Menu [options]{@link Menu#options}.
+ */
+var AMenu = new Class(/** @lends Menu# */{
+    Implements: [Options, Events],
+
+    /**
+     * Menu options.
+     * @type {Object}
+     *
+     * @property {string} [itemSelect = 'mouseenter'] Event name for selecting the menu item.
+     * @property {string} [deactivateMenu = 'mouseleave'] Event name for deactivating the menu.
+     */
+    options: {
+        itemSelect: 'mouseenter',
+        deactivateMenu: 'mouseleave'
+    },
+
+    /**
+     * Reference constructor for the submenu.
+     * @type {Function}
+     */
+    submenuRef: null,
+
+    // constructor
+    initialize: function(el, options) {
+        var self = this;
+
+        /**
+         * Root menu element.
+         * @type {Element}
+         */
+        this.element = $(el);
+        this.element.addEvent(this.options.deactivateMenu, function() {
+            self.deactivate();
+        });
+
+        this.setOptions(options);
+
+        this.setSubmenuConstructor();
+
+        /**
+         * Menu items.
+         * @type {Elements}
+         */
+        this.items = this.element.getChildren('.menu_item');
+        this.items.each(function(item) {
+            item.addEvent(self.options.itemSelect, function() {
+                self.selectItem(item);
+            });
+        });
+        this.items.each(function(item) {
+            if (item.hasClass('submenu')) {
+                self.createSubmenu(item);
+            }
+        });
+    },
+
+    /**
+     * Create submenu.
+     *
+     * @abstract
+     * @function
+     * @public
+     * @param {Element} submenu Submenu element.
+     */
+    createSubmenu: function(submenu) {
+        submenu.store('submenu', new this.submenuRef(submenu));
+    },
+
+    /**
+     * Activate the menu.
+     * @abstract
+     * @function
+     * @public
+     */
+    activate: function() {
+        this.element.addClass('active');
+    },
+
+    /**
+     * Deactivate the menu.
+     * @abstract
+     * @function
+     * @public
+     */
+    deactivate: function() {
+        this.element.removeClass('active');
+        this.items.removeClass('active');
+    },
+
+    /**
+     * Return true if the menu is visible, otherwise - false.
+     *
+     * @function
+     * @public
+     * @returns {boolean}
+     */
+    isActive: function() {
+        return this.element.hasClass('active');
+    },
+
+    /**
+     * Event handler. Select the item menu.
+     * @function
+     * @public
+     */
+    selectItem: function(item) {
+        this.items.removeClass('active');
+        item.addClass('active');
+
+        var submenu = item.retrieve('submenu');
+        if (submenu) {
+            submenu.activate();
+        }
+    },
+
+    /**
+     * Set the reference constructor for the submenu.
+     *
+     * @abstract
+     * @function
+     * @protected
+     * @param {Function} [ref = Menu] Reference constructor.
+     */
+    setSubmenuConstructor: function(ref) {
+        this.submenuRef = ref || Menu;
+    }.protect()
+});

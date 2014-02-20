595682622ed546ad21a74c5b60c6e7e36e9552e5
dexter <wildex1@gmail.com>
2014-02-20 17:09:33 +0200

    Button fix

---
+++ tests/phpunit/ButtonTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Tests also
+ * @code
+class Control;
+@endcode
+ * Class ButtonTest
+ */
+class ButtonTest extends PHPUnit_Framework_TestCase {
+
+    const CONTROL_XML = '<control id="add" title="BTN_PHPUNIT" type="button"/>';
+    /**
+     * @var button
+     */
+    private $control;
+
+    public function setUp() {
+        $xml = new SimpleXMLElement(self::CONTROL_XML);
+        $this->control = new Button('control');
+        $this->control->loadFromXml($xml);
+    }
+
+    public function testLoadFromXml() {
+        $this->assertEquals('BTN_PHPUNIT', $this->control->getTitle());
+        $this->assertEquals('button', $this->control->getType());
+        $this->assertEquals('add', $this->control->getID());
+    }
+
+    public function testBuild() {
+        $this->control->build();
+    }
+}

9c1a5301ee883cd0cfbb56f8c19a567ae127b43d
Pavel Dubenko <d.pavka@gmail.com>
2015-03-31 08:38:20 +0300

    QuryResult vs array  in QAL

---
+++ core/modules/share/gears/QueryResult.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * @file
+ * QueryResult 
+ *
+ * Contains the definition to:
+ * @code
+class QueryResult;
+ * @endcode
+ *
+ * @author dr.Pavka
+ * @copyright Energine 2015
+ *
+ * @version 1.0.0
+ */
+
+namespace Energine\share\gears;
+
+
+use Traversable;
+
+class QueryResult implements \IteratorAggregate, \ArrayAccess{
+    private $data;
+    function __construct($data) {
+        $this->data = $data;
+    }
+
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Retrieve an external iterator
+     * @link http://php.net/manual/en/iteratoraggregate.getiterator.php
+     * @return Traversable An instance of an object implementing <b>Iterator</b> or
+     * <b>Traversable</b>
+     */
+    public function getIterator() {
+        return new \ArrayIterator($this->data);
+    }
+    /**
+     * Get a data by key
+     *
+     * @param string The key data to retrieve
+     * @access public
+     */
+    public function &__get ($key) {
+        return $this->data[$key];
+    }
+
+    /**
+     * Assigns a value to the specified data
+     *
+     * @param string The data key to assign the value to
+     * @param mixed  The value to set
+     * @access public
+     */
+    public function __set($key,$value) {
+        $this->data[$key] = $value;
+    }
+
+    /**
+     * Whether or not an data exists by key
+     *
+     * @param string An data key to check for
+     * @access public
+     * @return boolean
+     * @abstracting ArrayAccess
+     */
+    public function __isset ($key) {
+        return isset($this->data[$key]);
+    }
+
+    /**
+     * Unsets an data by key
+     *
+     * @param string The key to unset
+     * @access public
+     */
+    public function __unset($key) {
+        unset($this->data[$key]);
+    }
+
+    /**
+     * Assigns a value to the specified offset
+     *
+     * @param string The offset to assign the value to
+     * @param mixed  The value to set
+     * @access public
+     * @abstracting ArrayAccess
+     */
+    public function offsetSet($offset,$value) {
+        if (is_null($offset)) {
+            $this->data[] = $value;
+        } else {
+            $this->data[$offset] = $value;
+        }
+    }
+
+    /**
+     * Whether or not an offset exists
+     *
+     * @param string An offset to check for
+     * @access public
+     * @return boolean
+     * @abstracting ArrayAccess
+     */
+    public function offsetExists($offset) {
+        return isset($this->data[$offset]);
+    }
+
+    /**
+     * Unsets an offset
+     *
+     * @param string The offset to unset
+     * @access public
+     * @abstracting ArrayAccess
+     */
+    public function offsetUnset($offset) {
+        if ($this->offsetExists($offset)) {
+            unset($this->data[$offset]);
+        }
+    }
+
+    /**
+     * Returns the value at specified offset
+     *
+     * @param string The offset to retrieve
+     * @access public
+     * @return mixed
+     * @abstracting ArrayAccess
+     */
+    public function offsetGet($offset) {
+        return $this->offsetExists($offset) ? $this->data[$offset] : null;
+    }
+}

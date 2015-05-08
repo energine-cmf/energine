4f7be0a7cb85b118bd45b786c10b256f379a159b
Pavel Dubenko <d.pavka@gmail.com>
2015-05-08 17:07:19 +0300

    draft

---
+++ core/modules/share/gears/ComponentData.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Содержит класс ComponentData
+ *
+ * @package energine
+ * @author dr.Pavka
+ * @copyright Energine 2015
+ */
+namespace Energine\share\gears;
+/**
+ * Proxy class for getting component data
+ *
+ * @package energine
+ * @author dr.Pavka
+ */
+class ComponentData extends Object {
+    public function __construct($name, $componentClassName, $params) {
+
+    }
+
+
+
+    public function get() {
+
+    }
+}

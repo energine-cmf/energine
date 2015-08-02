d2d4dfd1af86eb5bd877cbd21fb848ac9555ac63
Pavel Dubenko <d.pavka@gmail.com>
2015-08-02 19:41:04 +0300

    added tags param

--- core/modules/share/components/SitemapTree.php
+++ core/modules/share/components/SitemapTree.php
@@ -6,7 +6,7 @@
  * It contains the definition to:
  * @code
 class SitemapTree;
-@endcode
+ * @endcode
  *
  * @author dr.Pavka
  * @copyright Energine 2006
@@ -14,39 +14,81 @@ class SitemapTree;
  * @version 1.0.0
  */
 namespace Energine\share\components;
+
+use Energine\share\gears\TagManager;
 use Energine\share\gears\TreeBuilder;
+
 /**
  * Site map.
  *
  * @code
 class SitemapTree;
-@endcode
+ * @endcode
  */
-class SitemapTree extends DataSet {
+class SitemapTree extends DataSet
+{
+    /**
+     * @copydoc Component::defineParams
+     */
+    protected function defineParams()
+    {
+        return array_merge(
+            parent::defineParams(),
+            [
+                'tags' => false
+            ]
+        );
+    }
+
+    protected function main()
+    {
+        $this->setType(self::COMPONENT_TYPE_LIST);
+        parent::main();
+        if ($this->getDataDescription()->getFieldDescriptionByName('tags')) {
+            $m = new TagManager(
+                $this->getDataDescription(),
+                $this->getData(),
+                'share_sitemap'
+            );
+            $m->createFieldDescription();
+            $m->createField();
+        }
+    }
+
     /**
      * @copydoc DataSet::loadData
      */
     // Загружает данные о дереве разделов
-    protected function loadData() {
+    protected function loadData()
+    {
         $sitemap = E()->getMap();
         $res = $sitemap->getInfo();
+        $filteredIDs = [];
+
+        if ($this->getParam('tags'))
+            $filteredIDs =
+                TagManager::getFilter($this->getParam('tags'), 'share_sitemap');
 
         foreach ($res as $id => $info) {
-        	$result [] = array(
-        	   'Id' => $id,
-        	   'Pid' =>$info['Pid'],
-        	   'Name' => $info['Name'],
-        	   'Segment' => $sitemap->getURLByID($id)
-        	);
+            if (in_array($id, $filteredIDs) || !$info['Pid']) {
+                $result [] = array(
+                    'Id' => $id,
+                    'Pid' => $info['Pid'],
+                    'Name' => $info['Name'],
+                    'Segment' => $sitemap->getURLByID($id)
+                );
+            }
         }
+
         return $result;
     }
 
     /**
      * @copydoc DataSet::createBuilder
      */
-    protected function createBuilder() {
-        $builder  = new TreeBuilder();
+    protected function createBuilder()
+    {
+        $builder = new TreeBuilder();
         $builder->setTree(E()->getMap()->getTree());
         return $builder;
     }

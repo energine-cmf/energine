4cec7ec15e7cf7484cec6f7365996bb1862feea0
Pavel Dubenko <d.pavka@gmail.com>
2016-07-30 15:30:33 +0300

    по мелочи

--- core/modules/user/components/LoginForm.php
+++ core/modules/user/components/LoginForm.php
@@ -8,10 +8,10 @@
 class LoginForm;
  * @endcode
  *
- * @author dr.Pavka
+ * @author    dr.Pavka
  * @copyright Energine 2006
  *
- * @version 1.0.0
+ * @version   1.0.0
  */
 
 namespace Energine\user\components;
@@ -26,109 +26,116 @@ class LoginForm;
  * @endcode
  */
 class LoginForm extends DataSet implements SampleLoginForm {
-    /**
-     * @copydoc DataSet::__construct
-     */
-    public function __construct($name, array $params = null) {
-        if(!isset($params['state']))
-            $params['state'] = E()->getDocument()->user->isAuthenticated() ? 'showLogoutForm' : 'showLoginForm';
+	/**
+	 * @copydoc DataSet::__construct
+	 */
+	public function __construct( $name, array $params = null ) {
+		if ( ! isset( $params['state'] ) ) {
+			$params['state'] = E()->getDocument()->user->isAuthenticated() ? 'showLogoutForm' : 'showLoginForm';
+		}
 
-        parent::__construct($name, $params);
-        $this->setTitle($this->translate('TXT_LOGIN_FORM'));
-        $base = E()->getSiteManager()->getCurrentSite()->base;
-        if (strpos($currDomain = E()->getSiteManager()->getCurrentSite()->host, Primitive::getConfigValue('site.domain')) === false) {
-            $base = 'http://' . Primitive::getConfigValue('site.domain') . '/';
-        }
+		parent::__construct( $name, $params );
+		$this->setTitle( $this->translate( 'TXT_LOGIN_FORM' ) );
+		$base = E()->getSiteManager()->getCurrentSite()->base;
+		if ( strpos( $currDomain = E()->getSiteManager()->getCurrentSite()->host,
+				Primitive::getConfigValue( 'site.domain' ) ) === false
+		) {
+			$base = 'http://' . Primitive::getConfigValue( 'site.domain' ) . '/';
+		}
 
-        $this->setAction($base . 'auth.php' . ((isset($_SERVER['HTTP_REFERER'])) ? '' : '?return=' . (($return = $this->getParam('successAction')) ? $return : E()->getRequest()->getURI())), true);
-    }
+		$this->setAction( $base . 'auth.php' . ( ( isset( $_SERVER['HTTP_REFERER'] ) ) ? '' : '?return=' . ( ( $return = $this->getParam( 'successAction' ) ) ? $return : E()->getRequest()->getURI() ) ),
+			true );
+	}
 
-    /**
-     * @copydoc DataSet::defineParams
-     */
-    // Добавлен successAction - УРЛ на который происходит переадресация в случае успеха
-    protected function defineParams() {
-        return array_merge(
-            parent::defineParams(),
-            array(
-                'successAction' => false
-            )
-        );
-    }
+	/**
+	 * @copydoc DataSet::defineParams
+	 */
+	// Добавлен successAction - УРЛ на который происходит переадресация в случае успеха
+	protected function defineParams() {
+		return array_merge(
+			parent::defineParams(),
+			[
+				'successAction' => false,
+			]
+		);
+	}
 
-    /**
-     * Show login form.
-     */
-    public function showLoginForm() {
-        $this->prepare();
-        if (isset($_COOKIE[UserSession::FAILED_LOGIN_COOKIE_NAME])) {
-            $messageField = new FieldDescription('message');
-            $messageField->setType(FieldDescription::FIELD_TYPE_STRING);
-            $this->getDataDescription()->addFieldDescription($messageField);
-            $messageField->setRights(FieldDescription::FIELD_MODE_READ);
+	/**
+	 * Show login form.
+	 */
+	public function showLoginForm() {
+		$this->prepare();
+		if ( isset( $_COOKIE[ UserSession::FAILED_LOGIN_COOKIE_NAME ] ) ) {
+			$messageField = new FieldDescription( 'message' );
+			$messageField->setType( FieldDescription::FIELD_TYPE_STRING );
+			$this->getDataDescription()->addFieldDescription( $messageField );
+			$messageField->setRights( FieldDescription::FIELD_MODE_READ );
 
-            $messageField = new Field('message');
-            $messageField->addRowData($_COOKIE[UserSession::FAILED_LOGIN_COOKIE_NAME]);
-            $this->getData()->addField($messageField);
-            E()->getResponse()->deleteCookie(UserSession::FAILED_LOGIN_COOKIE_NAME);
-        }
+			$messageField = new Field( 'message' );
+			$messageField->addRowData( $_COOKIE[ UserSession::FAILED_LOGIN_COOKIE_NAME ] );
+			$this->getData()->addField( $messageField );
+			E()->getResponse()->deleteCookie( UserSession::FAILED_LOGIN_COOKIE_NAME );
+		}
 
-        //Во избежание появления empty рекордсета
-        $this->getData()->addField((new Field('username'))->setRowData(0, ''));
-        //Если есть информация о авторизации через фейсбук
-        foreach (array('auth.facebook', 'auth.vk') as $configSectionName) {
-            if ($this->getToolbar()) {
-                list($tbr) = array_values($this->getToolbar());
-                if ($ctrl = $tbr->getControlByID($configSectionName)) $ctrl->disable();
+		//Во избежание появления empty рекордсета
+		$this->getData()->addField( ( new Field( 'username' ) )->setRowData( 0, '' ) );
+		//Если есть информация о авторизации через фейсбук
+		foreach ( [ 'auth.facebook', 'auth.vk' ] as $configSectionName ) {
+			if ( $this->getToolbar() ) {
+				list( $tbr ) = array_values( $this->getToolbar() );
+				if ( $ctrl = $tbr->getControlByID( $configSectionName ) ) {
+					$ctrl->disable();
+				}
 
-                if ($ctrl && $this->getConfigValue($configSectionName)) {
-                    if ($appID = $this->getConfigValue($configSectionName . '.appID')) {
-                        $ctrl->setAttribute('appID', $appID);
-                        $ctrl->enable();
-                    }
+				if ( $ctrl && $this->getConfigValue( $configSectionName ) ) {
+					if ( $appID = $this->getConfigValue( $configSectionName . '.appID' ) ) {
+						$ctrl->setAttribute( 'appID', $appID );
+						$ctrl->enable();
+					}
 
-                }
-            }
-        }
-    }
+				}
+			}
+		}
+	}
 
 
-    /**
-     * Show logout form.
-     */
-    public function showLogoutForm() {
-        //$request = E()->getRequest();
-        //$this->setTitle($this->translate('TXT_LOGOUT'));
-        $this->addTranslation('TXT_USER_GREETING', 'TXT_USER_NAME', 'TXT_ROLE_TEXT');
-        //$this->setAction(E()->getSiteManager()->getCurrentSite()->base, true);
-        $this->prepare();
-        $data = [];
-        foreach (E()->UserGroup->getUserGroups($this->document->user->getID()) as $roleID) {
-            $tmp = E()->UserGroup->getInfo($roleID);
-            $data[] = $tmp['group_name'];
-        }
-	if (false===$this->getData()->isEmpty()) {
-            $this->getData()->getFieldByName('role_name')->setData(implode(', ', $data));
-        
-        }
-    }
+	/**
+	 * Show logout form.
+	 */
+	public function showLogoutForm() {
+		//$request = E()->getRequest();
+		//$this->setTitle($this->translate('TXT_LOGOUT'));
+		$this->addTranslation( 'TXT_USER_GREETING', 'TXT_USER_NAME', 'TXT_ROLE_TEXT' );
+		//$this->setAction(E()->getSiteManager()->getCurrentSite()->base, true);
+		$this->prepare();
+		$data = [ ];
+		foreach ( E()->UserGroup->getUserGroups( $this->document->user->getID() ) as $roleID ) {
+			$tmp    = E()->UserGroup->getInfo( $roleID );
+			$data[] = $tmp['group_name'];
+		}
+		if (!$this->getData()->isEmpty() && $this->getData()->getFieldByName( 'role_name' )) {
+			$this->getData()->getFieldByName( 'role_name' )->setData( implode( ', ', $data ) );
 
-    /**
-     * @copydoc DataSet::loadData
-     */
-    protected function loadData() {
-        $result = false;
-        switch ($this->getState()) {
-            case 'showLogoutForm':
-                foreach ($this->getDataDescription()->getFieldDescriptionList() as $fieldName) {
-                    $result[] = array($fieldName => $this->document->user->getValue($fieldName));
-                }
-                break;
-            default:
-                $result = parent::loadData();
-        }
-        return $result;
-    }
+		}
+	}
+
+	/**
+	 * @copydoc DataSet::loadData
+	 */
+	protected function loadData() {
+		$result = false;
+		switch ( $this->getState() ) {
+			case 'showLogoutForm':
+				foreach ( $this->getDataDescription()->getFieldDescriptionList() as $fieldName ) {
+					$result[] = [ $fieldName => $this->document->user->getValue( $fieldName ) ];
+				}
+				break;
+			default:
+				$result = parent::loadData();
+		}
+
+		return $result;
+	}
 }
 
 /**

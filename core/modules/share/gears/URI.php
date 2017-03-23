65d8964476b4ddd2f589f9fcca219c17c57d0522
pavka <d.pavka@gmail.com>
2017-03-23 09:41:45 +0200

    fix for HTTPS forwarded

--- core/modules/share/gears/URI.php
+++ core/modules/share/gears/URI.php
@@ -8,10 +8,10 @@
 final class URI;
  * @endcode
  *
- * @author 1m
+ * @author    1m
  * @copyright Energine
  *
- * @version 1.0.0
+ * @version   1.0.0
  */
 namespace Energine\share\gears;
 
@@ -21,265 +21,281 @@ namespace Energine\share\gears;
  * @final
  */
 final class URI extends Primitive {
-    /**
-     * Request scheme (protocol).
-     * @var string $scheme
-     */
-    private $scheme;
+	/**
+	 * Request scheme (protocol).
+	 * @var string $scheme
+	 */
+	private $scheme;
 
-    /**
-     * Host name.
-     * @var string $host
-     */
-    private $host;
+	/**
+	 * Host name.
+	 * @var string $host
+	 */
+	private $host;
 
-    /**
-     * Path.
-     * @var array $path
-     */
-    private $path;
+	/**
+	 * Path.
+	 * @var array $path
+	 */
+	private $path;
 
-    /**
-     * Query of parameters.
-     * @var string $query
-     */
-    private $query;
+	/**
+	 * Query of parameters.
+	 * @var string $query
+	 */
+	private $query;
 
-    /**
-     * Document fragment ID.
-     * @var string $fragment
-     */
-    private $fragment;
+	/**
+	 * Document fragment ID.
+	 * @var string $fragment
+	 */
+	private $fragment;
 
-    /**
-     * Trick for imitation of private constructor.
-     * @var bool $trick
-     */
-    private static $trick;
+	/**
+	 * Trick for imitation of private constructor.
+	 * @var bool $trick
+	 */
+	private static $trick;
 
-    /**
-     * Port number.
-     * @var int $port
-     */
-    private $port;
+	/**
+	 * Port number.
+	 * @var int $port
+	 */
+	private $port;
 
-    /**
-     * @param string $uri URI.
-     *
-     * @throws SystemException 'ERR_PRIVATE_CONSTRUCTOR'
-     */
-    public function __construct($uri) {
-        if (is_null(self::$trick)) {
-            throw new SystemException('ERR_PRIVATE_CONSTRUCTOR', SystemException::ERR_DEVELOPER);
-        }
-        $matches = [];
+	/**
+	 * @param string $uri URI.
+	 *
+	 * @throws SystemException 'ERR_PRIVATE_CONSTRUCTOR'
+	 */
+	public function __construct( $uri ) {
+		if ( is_null( self::$trick ) ) {
+			throw new SystemException( 'ERR_PRIVATE_CONSTRUCTOR', SystemException::ERR_DEVELOPER );
+		}
+		$matches = [];
 
-        if ($uri && ($matches = self::validate($uri))) {
-            $this->setScheme($matches[0]);
-            $this->setHost($matches[1]);
-            $this->setPort($matches[2]);
-            $this->setPath($matches[3]);
-            $this->setQuery(isset($matches[4]) ? $matches[4] : '');
-        } else {
-            $this->scheme = $this->host = $this->path = $this->query = $this->fragment = '';
-        }
-    }
+		if ( $uri && ( $matches = self::validate( $uri ) ) ) {
+			$this->setScheme( $matches[0] );
+			$this->setHost( $matches[1] );
+			$this->setPort( $matches[2] );
+			$this->setPath( $matches[3] );
+			$this->setQuery( isset( $matches[4] ) ? $matches[4] : '' );
+		} else {
+			$this->scheme = $this->host = $this->path = $this->query = $this->fragment = '';
+		}
+	}
 
-    //todo VZ: I think the using of throws is better than return false.
-    /**
-     * Validate URI.
-     * It returns an array of matched strings.
-     *
-     * @param string $uri URI
-     * @return array|bool
-     */
-    public static function validate($uri) {
-        $result = false;
-        if (preg_match('/^(\w+):\/\/([a-z0-9\.\-]+)\:?([0-9]{2,5})?(\/[^?]*)[\?]?(.*)?$/i', $uri, $matches) && count($matches) >= 5) {
-            array_shift($matches);
-            $result = $matches;
-        }
-        return $result;
-    }
+	//todo VZ: I think the using of throws is better than return false.
+	/**
+	 * Validate URI.
+	 * It returns an array of matched strings.
+	 *
+	 * @param string $uri URI
+	 *
+	 * @return array|bool
+	 */
+	public static function validate( $uri ) {
+		$result = false;
+		if ( preg_match( '/^(\w+):\/\/([a-z0-9\.\-]+)\:?([0-9]{2,5})?(\/[^?]*)[\?]?(.*)?$/i', $uri,
+				$matches ) && count( $matches ) >= 5
+		) {
+			array_shift( $matches );
+			$result = $matches;
+		}
 
-    /**
-     * Create URI.
-     *
-     * @param string $uriString URI string.
-     * @return URI
-     */
-    public static function create($uriString = '') {
-        self::$trick = true;
-        if (!$uriString) {
-            $host = Primitive::getConfigValue('site.domain');
-            $protocol = 'http';
-            $requestURI = '/';
-            if (!E()->Utils->is_PHP_CLI()) {
-                $host = explode(':', ((isset($_SERVER['HTTP_HOST'])) ? $_SERVER['HTTP_HOST'] : $_SERVER['SERVER_NAME']));
-                $protocol = (!empty($_SERVER['HTTPS']) ? 'https' : 'http');
-                $requestURI = $_SERVER['REQUEST_URI'];
-            }
+		return $result;
+	}
+
+	/**
+	 * Create URI.
+	 *
+	 * @param string $uriString URI string.
+	 *
+	 * @return URI
+	 */
+	public static function create( $uriString = '' ) {
+		self::$trick = true;
+		if ( ! $uriString ) {
+			$host       = Primitive::getConfigValue( 'site.domain' );
+			$protocol   = 'http';
+			$requestURI = '/';
+			if ( ! E()->Utils->is_PHP_CLI() ) {
+				$host       = explode( ':',
+					( ( isset( $_SERVER['HTTP_HOST'] ) ) ? $_SERVER['HTTP_HOST'] : $_SERVER['SERVER_NAME'] ) );
+				$protocol   = ( ( isset( $_SERVER['HTTPS'] ) &&
+				                  ( $_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1 ) ||
+				                  isset( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) &&
+				                  $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' ) ? 'https' : 'http' );
+				$requestURI = $_SERVER['REQUEST_URI'];
+			}
 
 
-            if (sizeof($host) == 1) {
-                $port = ($protocol == 'http') ? 80 : 443;
-                list($host) = $host;
-            } else {
-                list($host, $port) = $host;
-            }
+			if ( sizeof( $host ) == 1 ) {
+				$port = ( $protocol == 'http' ) ? 80 : 443;
+				list( $host ) = $host;
+			} else {
+				list( $host, $port ) = $host;
+			}
 
-            $uriString = $protocol . '://' . $host . ':' . $port . $requestURI;
-        }
-        return new URI($uriString);
-    }
+			$uriString = $protocol . '://' . $host . ':' . $port . $requestURI;
+		}
 
-    /**
-     * Set scheme (protocol)
-     *
-     * @param string $scheme Scheme.
-     */
-    public function setScheme($scheme) {
-        $this->scheme = strtolower($scheme);
-    }
+		return new URI( $uriString );
+	}
 
-    /**
-     * Get scheme (protocol).
-     *
-     * @return string
-     */
-    public function getScheme() {
-        return $this->scheme;
-    }
+	/**
+	 * Set scheme (protocol)
+	 *
+	 * @param string $scheme Scheme.
+	 */
+	public function setScheme( $scheme ) {
+		$this->scheme = strtolower( $scheme );
+	}
 
-    /**
-     * Set host name.
-     *
-     * @param string $host Host name.
-     */
-    public function setHost($host) {
-        $this->host = strtolower($host);
-    }
+	/**
+	 * Get scheme (protocol).
+	 *
+	 * @return string
+	 */
+	public function getScheme() {
+		return $this->scheme;
+	}
 
-    /**
-     * Get host name.
-     * @return string
-     */
-    public function getHost() {
-        return $this->host;
-    }
+	/**
+	 * Set host name.
+	 *
+	 * @param string $host Host name.
+	 */
+	public function setHost( $host ) {
+		$this->host = strtolower( $host );
+	}
 
-    /**
-     * Set port number.
-     * If @c $port is not defined, then the port @c 80 will be used.
-     *
-     * @param int $port Port number.
-     */
-    public function setPort($port) {
-        if (!$port) $port = 80;
-        $this->port = $port;
-    }
+	/**
+	 * Get host name.
+	 * @return string
+	 */
+	public function getHost() {
+		return $this->host;
+	}
 
-    /**
-     * Get port.
-     *
-     * @return int
-     */
-    public function getPort() {
-        return $this->port;
-    }
+	/**
+	 * Set port number.
+	 * If @c $port is not defined, then the port @c 80 will be used.
+	 *
+	 * @param int $port Port number.
+	 */
+	public function setPort( $port ) {
+		if ( ! $port ) {
+			$port = 80;
+		}
+		$this->port = $port;
+	}
 
-    /**
-     * Set path.
-     *
-     * @param string $path Path.
-     */
-    public function setPath($path) {
-        if (!is_array($path)) {
-            $path = array_values(array_diff(explode('/', $path), ['']));
-        }
-        $this->path = $path;
-    }
+	/**
+	 * Get port.
+	 *
+	 * @return int
+	 */
+	public function getPort() {
+		return $this->port;
+	}
 
-    /**
-     * Get path.
-     *
-     * @param boolean $asString Return as string?
-     * @return array|string
-     */
-    public function getPath($asString = true) {
-        $path = $this->path;
-        if ($asString) {
-            if (!empty($path)) {
-                $path = '/' . implode('/', $path) . '/';
-            } else {
-                $path = '/';
-            }
-        }
-        return $path;
-    }
+	/**
+	 * Set path.
+	 *
+	 * @param string $path Path.
+	 */
+	public function setPath( $path ) {
+		if ( ! is_array( $path ) ) {
+			$path = array_values( array_diff( explode( '/', $path ), [ '' ] ) );
+		}
+		$this->path = $path;
+	}
 
-    /**
-     * Get path segment with @c $pos index.
-     *
-     * @param int $pos Position
-     * @return string
-     */
-    public function getPathSegment($pos) {
-        if (isset($this->path[$pos])) {
-            return $this->path[$pos];
-        }
-        return '';
-    }
+	/**
+	 * Get path.
+	 *
+	 * @param boolean $asString Return as string?
+	 *
+	 * @return array|string
+	 */
+	public function getPath( $asString = true ) {
+		$path = $this->path;
+		if ( $asString ) {
+			if ( ! empty( $path ) ) {
+				$path = '/' . implode( '/', $path ) . '/';
+			} else {
+				$path = '/';
+			}
+		}
 
-    /**
-     * Set query.
-     *
-     * @param string $query Query.
-     */
-    public function setQuery($query) {
-        $this->query = strval($query);
-    }
+		return $path;
+	}
 
-    /**
-     * Get query.
-     *
-     * @return string
-     */
-    public function getQuery() {
-        return $this->query;
-    }
+	/**
+	 * Get path segment with @c $pos index.
+	 *
+	 * @param int $pos Position
+	 *
+	 * @return string
+	 */
+	public function getPathSegment( $pos ) {
+		if ( isset( $this->path[ $pos ] ) ) {
+			return $this->path[ $pos ];
+		}
 
-    /**
-     * Set document fragment.
-     *
-     * @param string $fragment Document fragment.
-     */
-    public function setFragment($fragment) {
-        $this->fragment = strval($fragment);
-    }
+		return '';
+	}
 
-    /**
-     * Get document fragment.
-     *
-     * @return string
-     */
-    public function getFragment() {
-        return $this->fragment;
-    }
+	/**
+	 * Set query.
+	 *
+	 * @param string $query Query.
+	 */
+	public function setQuery( $query ) {
+		$this->query = strval( $query );
+	}
 
-    /**
-     * Get URI as string.
-     *
-     * @return string
-     */
-    public function __toString() {
-        if (!empty($this->scheme) && !empty($this->host)) {
-            return $this->scheme . '://' . $this->host .
-            (empty($this->path) ? '/' : $this->getPath(true)) .
-            (empty($this->query) ? '' : '?' . $this->query) .
-            (empty($this->fragment) ? '' : '#' . $this->fragment);
-        } else {
-            return '';
-        }
-    }
+	/**
+	 * Get query.
+	 *
+	 * @return string
+	 */
+	public function getQuery() {
+		return $this->query;
+	}
+
+	/**
+	 * Set document fragment.
+	 *
+	 * @param string $fragment Document fragment.
+	 */
+	public function setFragment( $fragment ) {
+		$this->fragment = strval( $fragment );
+	}
+
+	/**
+	 * Get document fragment.
+	 *
+	 * @return string
+	 */
+	public function getFragment() {
+		return $this->fragment;
+	}
+
+	/**
+	 * Get URI as string.
+	 *
+	 * @return string
+	 */
+	public function __toString() {
+		if ( ! empty( $this->scheme ) && ! empty( $this->host ) ) {
+			return $this->scheme . '://' . $this->host .
+			       ( empty( $this->path ) ? '/' : $this->getPath( true ) ) .
+			       ( empty( $this->query ) ? '' : '?' . $this->query ) .
+			       ( empty( $this->fragment ) ? '' : '#' . $this->fragment );
+		} else {
+			return '';
+		}
+	}
 }
